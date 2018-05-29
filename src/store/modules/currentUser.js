40cfd75ac852ad85311c90a2d4fa5869a4e00ba5
Marc Salvat <salvat.marc@gmail.com>
2018-05-29 22:25:38 +0200

    Create vuex store architecture.

---
+++ src/store/modules/currentUser.js
@@ -0,0 +1,17 @@
+export default {
+  state: {
+    id: 0,
+    firstName: 'Jean',
+    lastName: 'Bombeur',
+    picture: '/public/img/logo.png'
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+}
