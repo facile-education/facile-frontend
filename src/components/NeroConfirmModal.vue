036a5e6886086a2ddbc17551eb13dcf3358912be
Marc Salvat <marc.salvat@pentila.com>
2018-08-30 17:09:10 +0200

    Add confirm modal.

---
+++ src/components/NeroConfirmModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <NeroWindow
+    :modal="true"
+    @close="onClose">
+    <span slot="header">
+      {{ title }}
+    </span>
+
+    <div slot="body">
+      {{ message }}
+    </div>
+
+    <NeroButton
+      slot="footer"
+      :label="confirmBtnLabel"
+      @click="onConfirm"/>
+  </NeroWindow>
+</template>
+
+<script>
+import NeroButton from '@/components/NeroButton'
+import NeroWindow from '@/components/NeroWindow'
+
+export default {
+  name: 'NeroConfirmModal',
+  components: {
+    NeroButton,
+    NeroWindow
+  },
+  computed: {
+    message () {
+      return this.$store.state.nero.confirmModal.message
+    },
+    title () {
+      return this.$store.state.nero.confirmModal.title
+    },
+    confirmBtnLabel () {
+      return this.$store.state.nero.confirmModal.buttonLabel
+    }
+  },
+  methods: {
+    onClose () {
+      this.$store.dispatch('nero/closeConfirmModal')
+    },
+    onConfirm () {
+      this.$store.state.nero.confirmModal.onConfirm()
+      this.onClose()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'src/assets/css/constants';
+
+</style>
