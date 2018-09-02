dd7cf9c15f539749e243b5c00d42a21ec1a2128d
Marc Salvat <salvat.marc@gmail.com>
2018-09-02 11:53:47 +0200

    Improve communication manager behavior.

---
+++ src/components/CommunicationManager/CMInternalRights.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="internal-rights">
+    <transition
+      appear
+      name="fade">
+      <div v-if="roleList">
+        <h4 v-t="'CommunicationManager.CMInternalRights.title'"/>
+        <p v-t="'CommunicationManager.CMInternalRights.informations'"/>
+        <NeroTabList>
+          <NeroTabItem
+            v-for="role in roleList"
+            :key="role.roleId"
+            :title="role.label">
+            <CMInternalRoleTab
+              :role="role"
+              :right-list="rightList"
+              @input="onInput"/>
+          </NeroTabItem>
+        </NeroTabList>
+        <NeroButton
+          :label="$t('CommunicationManager.CMInternalRights.save')"
+          @click="onSave"/>
+      </div>
+      <NeroSpinner v-else/>
+    </transition>
+  </div>
+</template>
+
+<script>
+import NeroButton from '@/components/NeroButton'
+import NeroSpinner from '@/components/NeroSpinner'
+import NeroTabItem from '@/components/NeroTabItem'
+import NeroTabList from '@/components/NeroTabList'
+import CMInternalRoleTab from '@/components/CommunicationManager/CMInternalRoleTab'
+
+export default {
+  name: 'CMInternalRights',
+  components: {
+    CMInternalRoleTab,
+    NeroButton,
+    NeroSpinner,
+    NeroTabItem,
+    NeroTabList
+  },
+  data () {
+    return {
+      rightList: {}
+    }
+  },
+  computed: {
+    savedRightList () {
+      return this.$store.state.communicationManager.CMInternalRights
+    },
+    roleList () {
+      return this.$store.state.communicationManager.roleList
+    }
+  },
+  watch: {
+    savedRightList (value) {
+      this.rightList = value
+    }
+  },
+  created () {
+    if (this.roleList === undefined) {
+      this.$store.dispatch('initCommunicationManagerRoleList')
+    }
+  },
+  methods: {
+    onSave () {
+      console.log(this.rightList)
+      var schoolId = this.$store.state.communicationManager.selectedSchoolId
+      this.$store.dispatch('updateSchoolInternalCommunicationRights',
+        { schoolId, rightList: this.rightList })
+    },
+    onInput (right) {
+      this.$set(this.rightList, right, !this.rightList[right])
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.internal-rights {
+  position: relative;
+  min-height: 100px;
+}
+</style>
