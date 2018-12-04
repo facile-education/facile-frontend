70017a0ad6cbb1a6d592d2ca7241251e012a3e72
Marc Salvat <marc.salvat@pentila.com>
2018-12-04 09:58:21 +0100

    First contact components.

---
+++ src/components/Contacts/ContactsToolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <NeroToolbar>
+    <NeroButton
+      icon="fa fa-bars"
+      @click="console.log('TODO')"
+    />
+  </NeroToolbar>
+</template>
+
+<script>
+import NeroButton from '@/components/NeroButton'
+import NeroToolbar from '@/components/NeroToolbar'
+
+export default {
+  name: 'ContactsToolbar',
+  components: {
+    NeroButton,
+    NeroToolbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
