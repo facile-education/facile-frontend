efa8c99e791e0668ce73933677d1d4add40264a5
Marc Salvat <marc.salvat@pentila.com>
2018-12-12 10:00:21 +0100

    Add comment to config file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/no-v-html': 'ignore'
+    'vue/no-v-html': 'ignore' // Ignore but we have to sanitize all of them
   },
   parserOptions: {
     parser: 'babel-eslint'
