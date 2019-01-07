3436011b91ecce8ce4e48f775e16c3675915e615
Marc Salvat <marc.salvat@pentila.com>
2019-01-07 15:11:21 +0100

    Add modern parameter to build.

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  baseUrl: '/nero/'
+  // baseUrl: '/nero/',
+  // configureWebpack: {
+  //   devServer: {
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*',
+  //       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  //       'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+  //     },
+  //     proxy: {
+  //       '/user': {
+  //         target: 'http://localhost:8580/',
+  //         changeOrigin: true
+  //       }
+  //     }
+  //   }
+  // }
+}
