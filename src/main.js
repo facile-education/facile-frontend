351234cc105a370c07dd5597143a735eeb01db8c
Marc Salvat <marc.salvat@pentila.com>
2019-02-11 08:52:55 +0100

    Catch logout error.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import store from './store/index'
 import i18n from './lang/lang.js'
 import VueMq from 'vue-mq'
 import './registerServiceWorker'
+import axios from 'axios'
+
+// Catch 401 unauthorized error and redirect user to public home page
+axios.interceptors.response.use(undefined, (error) => {
+  if (error.response.status === 401 && error.response.config && !error.response.config.__isRetryRequest) {
+    // if you ever get an unauthorized, logout the user
+    // this.$store.dispatch(AUTH_LOGOUT)
+    // Redirect
+    window.location.href = '/'
+  }
+
+  // Do something with response error
+  return Promise.reject(error)
+})
 
 Vue.config.productionTip = false
 Vue.use(VueMq, {
