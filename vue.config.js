556d121a7bb6545fde7c269ada2fa6cc618572df
Marc Salvat <marc.salvat@pentila.com>
2019-04-18 14:51:44 +0200

    Update project configuration files.

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-  baseUrl: '/nero/'
-  // configureWebpack: {
-  //   devServer: {
-  //     headers: {
-  //       'Access-Control-Allow-Origin': '*',
-  //       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-  //       'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-  //     },
-  //     proxy: {
-  //       '/user': {
-  //         target: 'http://localhost:8580/',
-  //         changeOrigin: true
-  //       }
-  //     }
-  //   }
-  // }
+  publicPath: '/nero/',
+  configureWebpack: {
+    devServer: {
+      public: 'localhost:8080',
+      disableHostCheck: true
+      // headers: {
+      //   'Access-Control-Allow-Origin': '*',
+      //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      //   'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+      // },
+      // proxy: {
+      //   '/user': {
+      //     target: 'http://localhost:8580/',
+      //     changeOrigin: true
+      //   }
+      // }
+    }
+  }
 }
