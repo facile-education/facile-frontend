60bbc8756640f9f98cbbed5fd0760c82249204f9
Marc Salvat <marc.salvat@pentila.com>
2019-05-06 16:20:19 +0200

    Rename preferences components.

---
+++ src/components/PreferencesWindow/BackupDetails.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <p><em>TODO : Hidden if no Dropbox && no Documents && no Agenda && no scheduler</em></p>
+    <p v-t="'PreferencesWindow.BackupDetails.explanationText'" />
+    <p v-t="'PreferencesWindow.BackupDetails.warningText'" />
+
+    <div>
+      {{ $t('PreferencesWindow.BackupDetails.selectionLabel') }}
+      <NeroCheckbox :label="$t('PreferencesWindow.BackupDetails.dropboxCheckboxLabel')" />
+      <NeroCheckbox :label="$t('PreferencesWindow.BackupDetails.workspaceCheckboxLabel')" />
+      <div>
+        <NeroCheckbox :label="$t('PreferencesWindow.BackupDetails.agendaCheckboxLabel')" />
+        <p v-t="'PreferencesWindow.BackupDetails.startDateLabel'" /> TODO Datepicker
+        <p v-t="'PreferencesWindow.BackupDetails.endDateLabel'" /> TODO Datepicker
+      </div>
+      <div>
+        <NeroCheckbox :label="$t('PreferencesWindow.BackupDetails.schedulerCheckboxLabel')" />
+        <p v-t="'PreferencesWindow.BackupDetails.startDateLabel'" /> TODO Datepicker
+        <p v-t="'PreferencesWindow.BackupDetails.endDateLabel'" /> TODO Datepicker
+      </div>
+    </div>
+
+    <NeroButton
+      :title="$t('PreferencesWindow.BackupDetails.copyButtonTitle')"
+      :label="$t('PreferencesWindow.BackupDetails.copyButtonLabel')"
+      @click="onCopy"
+    />
+  </div>
+</template>
+
+<script>
+import NeroButton from '@/components/Nero/NeroButton'
+import NeroCheckbox from '@/components/Nero/NeroCheckbox'
+
+export default {
+  name: 'BackupDetails',
+  components: {
+    NeroButton,
+    NeroCheckbox
+  },
+  methods: {
+    onCopy () {
+      console.log('on copy')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/PreferencesWindow/NotificationDetails.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p><em>TODO : Hidden if no Messaging || (no Dropbox && no News && no Documents & no Forums & no Agenda & no Groups)</em></p>
+    <p v-t="'PreferencesWindow.NotificationDetails.panelExplanationText'" />
+    <div>
+      {{ $t('PreferencesWindow.NotificationDetails.servicesLabel') }}
+
+      <NeroCheckbox :label="$t('PreferencesWindow.NotificationDetails.newsCheckboxLabel')" />
+      <NeroCheckbox :label="$t('PreferencesWindow.NotificationDetails.agendaCheckboxLabel')" />
+      <NeroCheckbox :label="$t('PreferencesWindow.NotificationDetails.dropboxCheckboxLabel')" />
+      <NeroCheckbox :label="$t('PreferencesWindow.NotificationDetails.workspaceCheckboxLabel')" />
+      <NeroCheckbox :label="$t('PreferencesWindow.NotificationDetails.forumsCheckboxLabel')" />
+      <NeroCheckbox :label="$t('PreferencesWindow.NotificationDetails.synchronizationCheckboxLabel')" />
+    </div>
+    <div>
+      {{ $t('PreferencesWindow.NotificationDetails.frequencyLabel') }}
+      <NeroDropdown :list="[]" />
+    </div>
+  </div>
+</template>
+
+<script>
+import NeroCheckbox from '@/components/Nero/NeroCheckbox'
+import NeroDropdown from '@/components/Nero/NeroDropdown'
+
+export default {
+  name: 'NotificationDetails',
+  components: {
+    NeroCheckbox,
+    NeroDropdown
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
