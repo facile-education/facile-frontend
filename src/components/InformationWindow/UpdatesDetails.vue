955eb4612b58de49ac6ef6b8b9c873b80a99aa48
vdona <vdona@pentila.com>
2019-05-14 14:04:18 +0200

    better NeroDropdown in updatesDetails

--- src/components/InformationWindow/UpdatesDetails.vue
+++ src/components/InformationWindow/UpdatesDetails.vue
@@ -3,20 +3,10 @@
     {{ $t('InformationWindow.UpdateDetails.version') }} :
     <!-- TODO afficher les updates dans le bon ordre -->
     <NeroDropdown
-      :list="testList"
-      display-field="text"
-      @dropdown-select="versionSelected"
-    />
-    <NeroDropdown
+      v-if="versionList"
       :list="versionList"
       display-field="versionNumber"
       @dropdown-select="selectedVersion"
-    />
-    <NeroDropdown
-      v-if="selectedVersionBis"
-      v-model="selectedVersionBis"
-      :list="versionList"
-      display-field="versionNumber"
     />
     <NeroButton
       class="addVersion"
@@ -25,7 +15,15 @@
       @click="addVersion"
     />
     <br>
-    <p> {{ versionDetails }} </p>
+    <p
+      v-if="versionDetails"
+      v-html="versionDetails"
+    />
+    <p
+      v-else
+    >
+      Unable to get version details
+    </p>
   </div>
 </template>
 
@@ -39,44 +37,12 @@ export default {
     NeroDropdown,
     NeroButton
   },
-  data () {
-    return {
-      testList: [
-        { text: 'Un', value: 'A' },
-        { text: 'Deux', value: 'B' },
-        { text: 'Trois', value: 'C' }
-      ],
-      text: "Ceci est le détail d'une version",
-      applicationTest: undefined
-    }
-  },
   computed: {
     versionList () {
       return this.$store.state.updates.versionList
     },
     versionDetails () {
       return this.$store.state.updates.versionDetails
-    },
-    selectedVersionBis: {
-      get () {
-        // recherche dans la liste des version si il y en a une qui correspond à application.versionId
-        if (this.versionList) {
-          return this.applicationTest
-        }
-        return undefined
-      },
-      // set application.versionId à la valeur courante du dropdown (que lors d'une sélection?)
-      set (value) {
-        this.applicationTest = value
-      }
-    },
-    selectedVersionTer (version) {
-      if (this.versionList) {
-        console.log(version.versionNumber)
-        return version
-      } else {
-        return undefined
-      }
     }
   },
   created () {
@@ -88,15 +54,10 @@ export default {
     addVersion () {
       this.$store.dispatch('nero/openUpdateEditionModal')
     },
-    versionSelected (version) {
-      console.log(version.value)
-      return version.value
-    },
     selectedVersion (version) {
       if (this.versionList) {
         // On donne à présent l'ordre de récupérer le contenu de la version
         this.$store.dispatch('updates/getVersionDetails', version) // (à encapsuler dans une méthode?)
-        console.log(version.versionNumber)
         return version
       } else {
         return undefined
@@ -114,7 +75,7 @@ export default {
 
 p {
     margin: 10px 0;
-    padding: 5px;
+    padding: 25px;
     border: 1px solid #999;
 }
 
