cb20a843fa99565aaa6b5f3cc50722ffb8803aa3
vdona <vdona@pentila.com>
2019-05-23 10:07:02 +0200

    rename VERSION_MANAGER_URL constant in INFORMATION_MANAGER_URL

--- src/api/constants.js
+++ src/api/constants.js
@@ -9,7 +9,7 @@ const DASHBOARD_MANAGER_URL = USER_PREFIX + '/admin-tableau-de-bord?p_p_id=gesti
 const DASHBOARD_URL = USER_PREFIX + '/tableau-de-bord?p_p_id=dashboard_WAR_dashboardportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage'
 const MENU_MANAGER_URL = USER_PREFIX + DEFAULT_APP + '?p_p_id=sideMenu_WAR_gestionMenusportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage'
 const PREFERENCES_URL = USER_PREFIX + DEFAULT_APP + '?p_p_id=preferenceHomePage_WAR_preferenceportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage'
-const VERSION_MANAGER_URL = USER_PREFIX + DEFAULT_APP + '?p_p_id=updateInformation_WAR_updateInformationportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage'
+const INFORMATION_MANAGER_URL = USER_PREFIX + DEFAULT_APP + '?p_p_id=updateInformation_WAR_updateInformationportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage'
 
 // TODO remove NODE server when useless
 const BASE_URL = 'http://193.48.120.116:3333'
@@ -21,5 +21,5 @@ export default {
   DASHBOARD_URL,
   MENU_MANAGER_URL,
   PREFERENCES_URL,
-  VERSION_MANAGER_URL
+  INFORMATION_MANAGER_URL
 }
