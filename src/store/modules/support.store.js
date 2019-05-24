a5e34530d49e0ff5fa60730d34cbd0bac2f88071
vdona <vdona@pentila.com>
2019-05-24 16:16:55 +0200

    form validation + screenshot

--- src/store/modules/support.store.js
+++ src/store/modules/support.store.js
@@ -3,10 +3,26 @@ import supportService from '@/api/support.service'
 export default {
   namespaced: true,
   state: {
+    fileList: []
   },
   mutations: {
+    addFile (state, payload) {
+      state.fileList.push(payload)
+    }
   },
   actions: {
+    addScreenShot ({ commit }, { image }) {
+      supportService.addScreenShot(image).then((data) => {
+        if (data.success) {
+          const file = {
+            type: 'Fichier',
+            id: data.fileId
+          }
+          commit('addFile', file)
+        } else {
+        }
+      })
+    },
     createMessage ({ commit }, { subjectField, contentField, mail, attachFiles, isUsurpationAllowed }) {
       supportService.createMessage(subjectField, contentField, mail, attachFiles, isUsurpationAllowed).then((data) => {
         if (data.success) {
