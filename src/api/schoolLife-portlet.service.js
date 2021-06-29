4a7947107ce93dc8c0e5a71ba1950357ef881258
vdona <vdona@pentila.com>
2021-06-29 14:53:20 +0200

    #3768 Fix WS url

--- src/api/schoolLife-portlet.service.js
+++ src/api/schoolLife-portlet.service.js
@@ -5,13 +5,13 @@ export default {
   getSchoolStudents
 }
 
-const SCHOOL_LIFE_PATH = 'schoollife-portlet/schoollifesessionstudent/'
+const SCHOOL_LIFE_PATH = '/schoollife-portlet'
 
 /**
  * Get the specified school's students filtered by name
  */
 function getSchoolStudents (schoolId, query) {
-  return axios.get(constants.JSON_WS_URL + SCHOOL_LIFE_PATH + '/get-school-students', {
+  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/get-school-students', {
     params: {
       // p_auth: getCookie('pauth'),
       schoolId: schoolId,
