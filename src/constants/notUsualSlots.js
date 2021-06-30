cf4b122c7054d1a71b24d45e04b5049c32bdaa41
vdona <vdona@pentila.com>
2021-06-30 17:28:34 +0200

    #3768 Add unusual slots in calendar on slot type selection

--- src/constants/notUsualSlots.js
+++ src/constants/notUsualSlots.js
@@ -1,33 +1,44 @@
 import i18n from '@/i18n'
 
+const firedType = 1
+const detentionType = 2
+const replayTestType = 3
+const tutoringType = 4
+const studyType = 5
+
 const slotTypes = [
   {
-    type: 'tutoring',
+    type: tutoringType,
     label: i18n.global.t('NotUsualSlots.tutoring'),
     color: '#8763CA'
   },
   {
-    type: 'study',
+    type: studyType,
     label: i18n.global.t('NotUsualSlots.study'),
     color: '#32AC71'
   },
   {
-    type: 'replayTest',
+    type: replayTestType,
     label: i18n.global.t('NotUsualSlots.replayTest'),
     color: '#3694B7'
   },
   {
-    type: 'detention',
+    type: detentionType,
     label: i18n.global.t('NotUsualSlots.detention'),
     color: '#EDA12A'
   },
   {
-    type: 'fired',
+    type: firedType,
     label: i18n.global.t('NotUsualSlots.fired'),
     color: '#f00'
   }
 ]
 
 export default {
+  firedType,
+  detentionType,
+  replayTestType,
+  tutoringType,
+  studyType,
   slotTypes
 }
