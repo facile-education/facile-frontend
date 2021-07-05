513b266b7dece73221d44b06a990abc5813c5452
vdona <vdona@pentila.com>
2021-07-05 17:23:06 +0200

    #3768 Remove persistent alert

--- src/store/modules/notUsualSlots.store.js
+++ src/store/modules/notUsualSlots.store.js
@@ -1,9 +1,46 @@
 import schoolLifeService from '@/api/schoolLife-portlet.service'
-import moment from 'moment'
+
+function getNonUsualSlots (store) {
+  schoolLifeService.getWeekSession(store.state.notUsualSlots.currentSlotType.type, store.state.notUsualSlots.displayedDates.startDate).then(
+    (data) => {
+      if (data.success) {
+        store.commit('setCurrentNonUsualSlots', data.sessions)
+      } else {
+        console.error('Cannot get slots for type ' + store.state.notUsualSlots.currentSlotType.type)
+      }
+    },
+    (err) => {
+      // TODO toastr
+      console.error(err)
+    }
+  )
+}
+
+function getStudentSessions (store) {
+  schoolLifeService.getStudentSessions(store.state.notUsualSlots.queriedUser, store.state.notUsualSlots.displayedDates.startDate, store.state.notUsualSlots.displayedDates.endDate).then(
+    (data) => {
+      if (data.success) {
+        store.commit('setUserSlots', data.sessions)
+      } else {
+        console.error('Cannot get user slots')
+      }
+    },
+    (err) => {
+      console.error(err)
+    }
+  )
+}
 
 export default {
   state: {
     currentSlotType: undefined,
+    displayedDates: {
+      startDate: undefined,
+      endDate: undefined
+    },
+    queriedUser: undefined,
+    currentStartDate: undefined,
+    currentEndDate: undefined,
     currentNonUsualSlots: [],
     userSlots: [],
     selectedSchool: undefined
@@ -11,6 +48,13 @@ export default {
   mutations: {
     setCurrentSlotType (state, slotType) {
       state.currentSlotType = slotType
+    },
+    setDisplayedDate (state, { startDate, endDate }) {
+      state.displayedDates.startDate = startDate
+      state.displayedDates.endDate = endDate
+    },
+    setQueriedUser (state, queriedUser) {
+      state.queriedUser = queriedUser
     },
     setSelectedSchool (state, payload) {
       state.selectedSchool = payload
@@ -25,36 +69,27 @@ export default {
   actions: {
     setCurrentSlotType ({ commit }, slotType) {
       commit('setCurrentSlotType', slotType)
-      schoolLifeService.getWeekSession(slotType.type, moment()).then(
-        (data) => {
-          if (data.success) {
-            commit('setCurrentNonUsualSlots', data.sessions)
-          } else {
-            console.error('Cannot get slots for type ' + slotType.label)
-          }
-        },
-        (err) => {
-          // TODO toastr
-          console.error(err)
-        }
-      )
+      getNonUsualSlots(this)
+    },
+    setQueriedUser ({ commit }, user) {
+      commit('setQueriedUser', user)
+      getStudentSessions(this)
+    },
+    setDisplayedDates ({ commit }, { startDate, endDate }) {
+      commit('setDisplayedDate', { startDate, endDate })
+      this.dispatch('refreshCalendar')
+    },
+    refreshCalendar () {
+      // Update displayedSlots
+      if (this.state.notUsualSlots.currentSlotType !== undefined) {
+        getNonUsualSlots(this)
+      }
+      if (this.state.notUsualSlots.queriedUser !== undefined) {
+        getStudentSessions(this)
+      }
     },
     setSelectedSchool ({ commit }, selectedSchool) {
       commit('setSelectedSchool', selectedSchool)
-    },
-    getStudentSessions ({ commit }, { user, minDate, maxDate }) {
-      schoolLifeService.getStudentSessions(user, minDate, maxDate).then(
-        (data) => {
-          if (data.success) {
-            commit('setUserSlots', data.sessions)
-          } else {
-            console.error('Cannot get user slots')
-          }
-        },
-        (err) => {
-          console.error(err)
-        }
-      )
     },
     resetUserSlots ({ commit }) {
       commit('setUserSlots', [])
