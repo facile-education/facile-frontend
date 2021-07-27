421316f3f117264a44a2e61db8edd4b4b38047ed
vdona <vdona@pentila.com>
2021-07-27 10:12:13 +0200

    #3768 [notUsualSlots] Pass by JSPPortlet instead of WS

--- src/api/schoolLife-portlet.service.js
+++ src/api/schoolLife-portlet.service.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import constants from '@/api/constants'
+// import constants from '@/api/constants'
 
 export default {
   getSchoolStudents,
@@ -20,15 +20,18 @@ export default {
   markStudentsPresent
 }
 
-const SCHOOL_LIFE_PATH = '/schoollife-portlet'
+// const SCHOOL_LIFE_PATH = '/schoollife-portlet'
+const SCHOOL_LIFE_URL = '/group/14924102/schoollife?p_p_id=schoollife_WAR_schoollifeportlet&p_p_lifecycle=2&p_p_state=exclusive&p_p_mode=view&&p_p_cacheability=cacheLevelPage&controlPanelCategory=portlet_schoollife_WAR_schoollifeportlet'
 
 /**
  * Get the specified school's students filtered by name
  */
 function getSchoolStudents (schoolId, query) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/get-school-students', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/get-school-students', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
       // p_auth: getCookie('pauth'),
+      cmd: 'get-school-students',
       schoolId: schoolId,
       search: query
     }
@@ -39,9 +42,10 @@ function getSchoolStudents (schoolId, query) {
  * Get the specified school's teachers filtered by name
  */
 function getSchoolTeachers (schoolId, query) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/get-teachers', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/get-teachers', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
-      // p_auth: getCookie('pauth'),
+      cmd: 'get-teachers',
       schoolId: schoolId,
       search: query
     }
@@ -52,9 +56,10 @@ function getSchoolTeachers (schoolId, query) {
  * Create a slot of a given type in the school. (return only success field)
  */
 function createSlot (schoolId, startDateStr, day, startHour, endHour, teacherId, type, room, capacity) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/create-slot', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/create-slot', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
-      // p_auth: getCookie('pauth'),
+      cmd: 'create-slot',
       schoolId: schoolId,
       startDateStr: startDateStr,
       day: day,
@@ -72,8 +77,10 @@ function createSlot (schoolId, startDateStr, day, startHour, endHour, teacherId,
  * Update a slot based on it id
  */
 function updateSlot (slotId, startDateStr, day, startHour, endHour, teacherId, type, room, capacity) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/edit-slot', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/edit-slot', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'edit-slot',
       schoollifeSessionId: slotId,
       startDateStr: startDateStr,
       newDay: day,
@@ -91,8 +98,10 @@ function updateSlot (slotId, startDateStr, day, startHour, endHour, teacherId, t
  * Delete a slot and all the sessions concerns by a slot after a given date
  */
 function deleteSlot (slotId, startDateStr) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/delete-slot', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifeslot/delete-slot', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'delete-slot',
       schoollifeSessionId: slotId,
       startDateStr: startDateStr
     }
@@ -103,8 +112,10 @@ function deleteSlot (slotId, startDateStr) {
  * Get the student's slots in the specified amount of time
  */
 function getStudentSessions (student, minDate, maxDate) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/get-student-sessions', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/get-student-sessions', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'get-student-sessions',
       studentId: student.studentId,
       minDateStr: minDate.format('YYYY/MM/DD HH:mm'),
       maxDateStr: maxDate.format('YYYY/MM/DD HH:mm')
@@ -117,8 +128,10 @@ function getStudentSessions (student, minDate, maxDate) {
  * Fields: success, candidateSessions
  */
 function getCandidateSessions (student, currentSlotId) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/get-candidate-sessions', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/get-candidate-sessions', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'get-candidate-sessions',
       studentId: student.studentId,
       schoollifeSessionId: currentSlotId
     }
@@ -129,8 +142,10 @@ function getCandidateSessions (student, currentSlotId) {
  * Get the unusual slots of a week, filtered by type
  */
 function getWeekSession (slotType, currentDate) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesession/get-week-sessions', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesession/get-week-sessions', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'get-week-sessions',
       type: slotType,
       currentDateStr: currentDate.format('YYYY/MM/DD HH:mm')
     }
@@ -141,8 +156,10 @@ function getWeekSession (slotType, currentDate) {
  * Get the students registered in a slot (fields: success, members)
  */
 function getSessionMembers (slotId) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/get-session-members', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/get-session-members', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'get-session-members',
       schoollifeSessionId: slotId
     }
   }).then(response => response.data)
@@ -152,8 +169,10 @@ function getSessionMembers (slotId) {
  * Register student in a not usual slot (fields: success)
  */
 function registerStudent (student, slotId, comment, notifyParents, replayTestSubject) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/register-student', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/register-student', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'register-student',
       studentId: student.studentId,
       schoollifeSessionId: slotId,
       comment: comment,
@@ -167,8 +186,10 @@ function registerStudent (student, slotId, comment, notifyParents, replayTestSub
  * UnRegister student from a not usual slot (fields: success)
  */
 function unRegisterStudent (student, slotId, comment, notifyParents, allSessions) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/unregister-student', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/unregister-student', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'unregister-student',
       studentId: student.studentId,
       schoollifeSessionId: slotId,
       comment: comment,
@@ -182,8 +203,10 @@ function unRegisterStudent (student, slotId, comment, notifyParents, allSessions
  * Register student firing (fields: success)
  */
 function registerFiring (slotId, student, sourceSessionId, sourceTeacherId) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/register-student-renvoi', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/register-student-renvoi', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'register-student-renvoi',
       schoollifeSessionId: slotId,
       studentId: student.studentId,
       sourceSessionId: sourceSessionId,
@@ -196,8 +219,10 @@ function registerFiring (slotId, student, sourceSessionId, sourceTeacherId) {
  * unRegisterFiring student firing (fields: success)
  */
 function unRegisterFiring (slotId, student) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/unregister-student-renvoi', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/unregister-student-renvoi', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'unregister-student-renvoi',
       schoollifeSessionId: slotId,
       studentId: student.studentId
     }
@@ -208,15 +233,20 @@ function unRegisterFiring (slotId, student) {
  * Get pending firing (fields: success, pendingRenvois)
  */
 function getPendingFirings () {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/get-pending-renvois').then(response => response.data)
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/get-pending-renvois').then(response => response.data)
+  return axios.get(SCHOOL_LIFE_URL, {
+    cmd: 'get-pending-renvois'
+  }).then(response => response.data)
 }
 
 /**
  * Set the reason of a firing (fields: success)
  */
 function setFiringReason (slotId, studentId, reason) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/set-renvoi-reason', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/renvoi/set-renvoi-reason', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'set-renvoi-reason',
       schoollifeSessionId: slotId,
       studentId: studentId,
       reason: reason
@@ -228,8 +258,10 @@ function setFiringReason (slotId, studentId, reason) {
  * StudentsPresence is a json containing all the students of a slot with a boolean 'isPresent' (fields: success)
  */
 function markStudentsPresent (slotId, studentsPresence) {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/mark-students-present', {
+  // return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/schoollifesessionstudent/mark-students-present', {
+  return axios.get(SCHOOL_LIFE_URL, {
     params: {
+      cmd: 'mark-students-present',
       schoollifeSessionId: slotId,
       studentsPresence: studentsPresence
     }
