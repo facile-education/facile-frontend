ffbe6fa0e52153773a40ea77d6c8517d61f3016f
vdona <vdona@pentila.com>
2021-07-28 15:12:13 +0200

    #3768 [notUsualSlots tests] Mobile navigation tests

--- tests/e2e/integration/HorairesHorsCadres/navigation.spec.js
+++ tests/e2e/integration/HorairesHorsCadres/navigation.spec.js
@@ -1,5 +1,20 @@
 import { now, url, slotTypes } from '../../support/constants/horairesHorsCadres'
 
+const checkSlotSelectionMenu = () => {
+  cy.get('[data-test=user-completion-input]').should('not.exist')
+  cy.get('.fc').should('not.exist')
+  cy.get('.date-picker').should('not.exist')
+
+  // Check all the slot types are here
+  cy.get('[data-test*=slot-type-item]').should('have.length', 5).parent().within(() => {
+    cy.get('[data-test=slot-type-item-' + slotTypes.tutoring.type + ']').should('exist')
+    cy.get('[data-test=slot-type-item-' + slotTypes.study.type + ']').should('exist')
+    cy.get('[data-test=slot-type-item-' + slotTypes.replayTest.type + ']').should('exist')
+    cy.get('[data-test=slot-type-item-' + slotTypes.detention.type + ']').should('exist')
+    cy.get('[data-test=slot-type-item-' + slotTypes.fired.type + ']').should('exist')
+  })
+}
+
 describe('desktop navigation', () => {
   beforeEach(() => {
     cy.logout()
@@ -9,17 +24,7 @@ describe('desktop navigation', () => {
 
   it('contains slot selection', () => {
     // At the beginning (when no slot type is selected), there is no display of calendar and user selection
-    cy.get('[data-test=user-completion-input]').should('not.exist')
-    cy.get('.fc').should('not.exist')
-
-    // Check all the slot types are here
-    cy.get('[data-test*=slot-type-item]').should('have.length', 5).parent().within(() => {
-      cy.get('[data-test=slot-type-item-' + slotTypes.tutoring.type + ']').should('exist')
-      cy.get('[data-test=slot-type-item-' + slotTypes.study.type + ']').should('exist')
-      cy.get('[data-test=slot-type-item-' + slotTypes.replayTest.type + ']').should('exist')
-      cy.get('[data-test=slot-type-item-' + slotTypes.detention.type + ']').should('exist')
-      cy.get('[data-test=slot-type-item-' + slotTypes.fired.type + ']').should('exist')
-    })
+    checkSlotSelectionMenu()
   })
 
   for (const attr in slotTypes) {
@@ -39,6 +44,53 @@ describe('desktop navigation', () => {
         cy.contains(slot.teacherNameAtWednesdaySlot).should('be.visible')
         // cy.contains('Capacité').should('be.visible') // TODO when refactoring capacity
       })
+    })
+  }
+})
+
+describe('mobile navigation', () => {
+  beforeEach(() => {
+    cy.logout()
+    cy.clock(now.toDate().getTime())
+    cy.viewport('iphone-5')
+    cy.login(url)
+  })
+
+  it('contains slot selection', () => {
+    // At the beginning (when no slot type is selected), there is no display of calendar and user selection
+    checkSlotSelectionMenu()
+  })
+
+  for (const attr in slotTypes) {
+    const slot = slotTypes[attr]
+    it('select ' + slot.label, () => {
+      // Select tutoring type
+      cy.get('[data-test=slot-type-item-' + slot.type + ']').click()
+
+      // User selection and Calendar should appear
+      cy.get('[data-test=user-completion-input]').should('be.visible')
+      cy.get('.toolbar [data-test=slot-type-item-' + slot.type + ']').should('be.visible').as(slot.label)
+      cy.get('.toolbar .date-picker').should('be.visible')
+      cy.get('.toolbar [data-test=user-completion-input]').should('be.visible')
+
+      // Calendar should be visible with today date only
+      cy.get('.fc').should('be.visible')
+      cy.contains(now.format('dddd DD/MM')).should('exist')
+      cy.contains(now.add(1, 'day').format('dddd DD/MM')).should('not.exist')
+      cy.contains(now.subtract(1, 'day').format('dddd DD/MM')).should('not.exist')
+
+      // Check events number
+      cy.get('.fc-timegrid-event').should('have.length', 1)
+      // Check event type teacher is displayed
+      cy.get('.fc-day-wed > .fc-timegrid-col-frame > :nth-child(2) >> .fc-timegrid-event').within(() => {
+        cy.contains(slot.label).should('be.visible')
+        cy.contains(slot.teacherNameAtWednesdaySlot).should('be.visible')
+        // cy.contains('Capacité').should('be.visible') // TODO when refactoring capacity
+      })
+
+      // Click on slotType to go back on slot selection
+      cy.get('@' + slot.label).click()
+      checkSlotSelectionMenu()
     })
   }
 })
