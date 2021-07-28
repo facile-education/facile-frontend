d4fa1e69fb5fdd8404283cb2227debe09ed04ef0
Marc Salvat <salvat.marc@gmail.com>
2021-07-28 18:09:26 +0200

    #3766 - Change backend call to WS.

--- src/api/userManagement.service.js
+++ src/api/userManagement.service.js
@@ -2,14 +2,28 @@ import axios from 'axios'
 import constants from '@/api/constants'
 
 export default {
+  getSchoolUsers,
   getSubjects
 }
 
-const SCHOOL_LIFE_PATH = '/gestionUtilisateurs-portlet'
+const USER_MANAGEMENT_PATH = '/gestionUtilisateurs-portlet'
 
 /**
  * Get the specified school's students filtered by name
  */
 function getSubjects () {
-  return axios.get(SCHOOL_LIFE_PATH + constants.JSON_WS_URL + '/subject/get-subjects').then(response => response.data)
+  return axios.get(USER_MANAGEMENT_PATH + constants.JSON_WS_URL + '/subject/get-subjects').then(response => response.data)
+}
+
+/**
+ * Get the specified school's users filtered by name
+ */
+function getSchoolUsers (schoolId, query) {
+  return axios.get(constants.JSON_WS_URL + USER_MANAGEMENT_PATH + '.usersearch/get-school-student-teacher-list', {
+    params: {
+      // p_auth: getCookie('pauth'),
+      schoolId: schoolId,
+      search: query
+    }
+  }).then(response => response.data)
 }
