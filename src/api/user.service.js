544faada5b6a15901a3c2191a7e1633121af6ef2
Marc Salvat <salvat.marc@gmail.com>
2021-08-27 10:12:38 +0200

    #3801 - Move user infos JSPPortlet commands to WS.

--- src/api/user.service.js
+++ src/api/user.service.js
@@ -1,35 +1,23 @@
 import axios from 'axios'
 import constants from '@/api/constants'
-import PentilaUtils from 'pentila-utils'
+// import PentilaUtils from 'pentila-utils'
 
 export default {
   getPersonalDetails,
-  getServiceList,
-  getUserInformations,
-  removePicture,
-  updateInterfacePreferences,
-  uploadProfilePicture
+  getUserInformations
+  // removePicture,
+  // updateInterfacePreferences,
+  // uploadProfilePicture
 }
+
+const PREF_PATH = '/preference-portlet.'
 
 /**
  * Get user details for preferences modal window
  */
 function getPersonalDetails () {
-  return axios.get(constants.PREFERENCES_URL, {
-    params: {
-      cmd: 'getPersonnalDetails'
-    }
-  }).then(response => response.data)
-}
-
-/**
- * Get User's service list
- */
-function getServiceList () {
-  return axios.get(constants.INFORMATION_MANAGER_URL, {
-    params: {
-      cmd: 'getServiceList'
-    }
+  return axios.get(constants.JSON_WS_URL + PREF_PATH + 'userproperties/get-personnal-details', {
+    params: {}
   }).then(response => response.data)
 }
 
@@ -37,51 +25,49 @@ function getServiceList () {
  * Get global user informations
  */
 function getUserInformations () {
-  return axios.get(constants.DASHBOARD_URL, {
-    params: {
-      cmd: 'getUserInfos'
-    }
+  return axios.get(constants.JSON_WS_URL + PREF_PATH + 'userproperties/get-user-infos', {
+    params: {}
   }).then(response => response.data)
 }
 
-/**
- * Remove user portrait
- */
-function removePicture () {
-  return axios.get(constants.PREFERENCES_URL, {
-    params: {
-      cmd: 'removePicture'
-    }
-  }).then(response => response.data)
-}
+// /**
+//  * Remove user portrait
+//  */
+// function removePicture () {
+//   return axios.get(constants.PREFERENCES_URL, {
+//     params: {
+//       cmd: 'removePicture'
+//     }
+//   }).then(response => response.data)
+// }
 
-/**
- * Update language, theme color and default menu status
- * @param {*} preferences
- */
-function updateInterfacePreferences (preferences) {
-  return axios.post(constants.PREFERENCES_URL, PentilaUtils.URL.params({
-    cmd: 'editInterface',
-    lang: preferences.language,
-    hideMenu: preferences.isMenuHidden,
-    themeColor: preferences.themeColor
-  }),
-  {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    }
-  }).then(response => response.data)
-}
+// /**
+//  * Update language, theme color and default menu status
+//  * @param {*} preferences
+//  */
+// function updateInterfacePreferences (preferences) {
+//   return axios.post(constants.PREFERENCES_URL, PentilaUtils.URL.params({
+//     cmd: 'editInterface',
+//     lang: preferences.language,
+//     hideMenu: preferences.isMenuHidden,
+//     themeColor: preferences.themeColor
+//   }),
+//   {
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+//     }
+//   }).then(response => response.data)
+// }
 
-/**
- * Update user profile picture
- * @param {*} formData
- */
-function uploadProfilePicture (formData) {
-  return axios.post(constants.PREFERENCES_URL + '&cmd=uploadPicture', formData,
-    {
-      headers: {
-        'Content-Type': undefined
-      }
-    }).then(response => response.data)
-}
+// /**
+//  * Update user profile picture
+//  * @param {*} formData
+//  */
+// function uploadProfilePicture (formData) {
+//   return axios.post(constants.PREFERENCES_URL + '&cmd=uploadPicture', formData,
+//     {
+//       headers: {
+//         'Content-Type': undefined
+//       }
+//     }).then(response => response.data)
+// }
