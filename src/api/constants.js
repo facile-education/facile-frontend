9016576355f3e90a6ac6017754596860cd5f79b3
Marc Salvat <salvat.marc@gmail.com>
2021-09-01 16:53:42 +0200

    Update appManager userGroupId.

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,6 @@
 const JSON_WS_URL = '/api/jsonws'
 
-const APPLICATION_MANAGER_URL = '/group/79867532/gestionapplications?p_p_id=gestionApps_WAR_gestionApplicationsportlet&p_p_lifecycle=2&p_p_state=exclusive&p_p_mode=view&p_p_cacheability=cacheLevelPage&controlPanelCategory=portlet_gestionApps_WAR_gestionApplicationsportlet'
+const APPLICATION_MANAGER_URL = '/group/313701/applications?p_p_id=gestionApps_WAR_gestionApplicationsportlet&p_p_lifecycle=2&p_p_state=exclusive&p_p_mode=view&p_p_cacheability=cacheLevelPage&controlPanelCategory=portlet_gestionApps_WAR_gestionApplicationsportlet'
 const COMMUNICATION_MANAGER_URL = '/group/3726289/administrationtools?p_p_id=administrationTools_WAR_administrationToolsportlet&p_p_lifecycle=2&p_p_state=exclusive&p_p_mode=view&p_p_cacheability=cacheLevelPage&controlPanelCategory=portlet_administrationTools_WAR_administrationToolsportlet'
 const DASHBOARD_MANAGER_URL = '/group/4445511/admin-tableau-de-bord?p_p_id=gestionDashboard_WAR_gestionDashboardportlet&p_p_lifecycle=2&p_p_state=exclusive&p_p_mode=view&p_p_cacheability=cacheLevelPage&controlPanelCategory=portlet_gestionDashboard_WAR_gestionDashboardportlet'
 
