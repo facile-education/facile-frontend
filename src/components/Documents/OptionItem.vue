06445d7e006394afe9aeb72dd1f8ef908e919c7f
vdona <vdona@pentila.com>
2021-10-14 11:45:24 +0200

    #3871 [Documents] Display subMenu options

--- src/components/Documents/OptionItem.vue
+++ src/components/Documents/OptionItem.vue
@@ -2,7 +2,7 @@
   <div
     class="option-item"
     :title="option.title"
-    @click="optionClicked"
+    @click="handleClick"
   >
     <img
       :src="option.icon"
@@ -11,13 +11,27 @@
     <div class="title">
       {{ option.title }}
     </div>
+    <img
+      v-if="option.subMenu"
+      class="arrow-down"
+      src="@assets/arrow-down.svg"
+      alt=""
+    >
   </div>
+
+  <ContextMenu
+    v-if="isContextMenuDisplayed && isAContextMenuDisplayed"
+    @chooseOption="subMenuOptionClicked"
+    @close="isContextMenuDisplayed=false"
+  />
 </template>
 
 <script>
 
+import ContextMenu from '@components/ContextMenu/ContextMenu'
 export default {
   name: 'OptionItem',
+  components: { ContextMenu },
   props: {
     option: {
       type: Object,
@@ -25,19 +39,51 @@ export default {
     }
   },
   emits: ['optionClicked'],
+  data () {
+    return {
+      isContextMenuDisplayed: false
+    }
+  },
+  computed: {
+    isAContextMenuDisplayed () {
+      return this.$store.state.contextMenu.isAContextMenuDisplayed
+    }
+  },
   methods: {
-    optionClicked () {
-      this.$emit('optionClicked', this.option)
+    handleClick (event) {
+      if (this.option.subMenu) {
+        this.openSubMenu(event)
+      } else {
+        this.$emit('optionClicked', this.option)
+      }
+    },
+    subMenuOptionClicked (option) {
+      this.isContextMenuDisplayed = false
+      this.$store.dispatch('contextMenu/closeMenus')
+      this.$emit('optionClicked', option)
+    },
+    openSubMenu (event) {
+      if (!this.isAContextMenuDisplayed) {
+        this.isContextMenuDisplayed = true
+        this.$store.dispatch('contextMenu/openContextMenu',
+          {
+            event: event,
+            options: this.option.subMenu
+          })
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "@design";
+
 .option-item {
   display: flex;
   cursor: pointer;
   align-items: center;
+  height: 100%;
   margin: 0 5px;
 
   img {
@@ -48,6 +94,17 @@ export default {
 
   .title {
     font-size: 0.875em;
+  }
+
+  .arrow-down {
+    width: 15px;
+    height: 15px;
+    padding: 0;
+    margin-left: 5px;
+  }
+
+  &:hover {
+    background-color: $color-hover-bg;
   }
 }
 </style>
