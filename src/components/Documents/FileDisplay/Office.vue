3783d563ff7b57f2e818b84e4ca1bee51c230f77
vdona <vdona@pentila.com>
2021-12-14 12:00:17 +0100

    #3904 [Documents] Add Office component

---
+++ src/components/Documents/FileDisplay/Office.vue
@@ -0,0 +1,31 @@
+<template>
+  <iframe
+    :src="src"
+    class="media"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Office',
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  beforeUnmount () {
+    // TODO
+    // DocumentService.removeLoolToken(this.token).then(() => {
+    //
+    // })
+  }
+}
+</script>
+
+<style scoped>
+.media {
+  height: 100%;
+  width: 100%;
+}
+</style>
