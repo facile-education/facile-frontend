5703e4512461a8155515b347c156100b53a1b08e
vdona <vdona@pentila.com>
2022-06-22 08:41:51 +0200

    Add hidden items management in breadcrumb

--- src/components/Documents/Breadcrumb.vue
+++ src/components/Documents/Breadcrumb.vue
@@ -3,18 +3,23 @@
     class="breadcrumb"
     data-test="breadcrumb"
   >
+    <HiddenItems
+      v-if="hiddenElements.length > 0 && (!mq.phone && !mq.tablet)"
+      :items="hiddenElements"
+      @optionClicked="changeDir"
+    />
     <template
-      v-for="(folder, index) in breadcrumb"
+      v-for="(folder, index) in displayableBreadcrumb"
       :key="folder.id"
     >
       <BreadCrumbItem
         :folder="folder"
         :is-first-element="index === 0"
-        :is-current-folder="index === breadcrumb.length-1"
+        :is-current-folder="index === displayableBreadcrumb.length-1"
         @click-back="goInParentFolder(index)"
       />
       <img
-        v-show="(!mq.phone && !mq.tablet) && index < (breadcrumb.length - 1)"
+        v-show="(!mq.phone && !mq.tablet) && index < (displayableBreadcrumb.length - 1)"
         class="chevron"
         src="@assets/icon_big_arrow.svg"
         alt=""
@@ -25,12 +30,31 @@
 
 <script>
 import BreadCrumbItem from '@components/Documents/BreadCrumbItem'
+import HiddenItems from '@components/Base/HiddenItems'
 export default {
-  components: { BreadCrumbItem },
+  components: { HiddenItems, BreadCrumbItem },
   inject: ['mq'],
   computed: {
     breadcrumb () {
       return this.$store.state.documents.breadcrumb
+    },
+    displayableBreadcrumb () {
+      return this.breadcrumb.slice(-4) // Only display the fourth elements of the breadcrumb
+    },
+    hiddenElements () {
+      const hiddenElements = []
+      let find = false
+      this.breadcrumb.forEach(originalElem => {
+        this.displayableBreadcrumb.forEach(displayedElement => {
+          if (displayedElement.id === originalElem.id) {
+            find = true
+          }
+        })
+        if (!find) {
+          hiddenElements.push({ ...originalElem, title: originalElem.name })
+        }
+      })
+      return hiddenElements
     }
   },
   methods: {
@@ -40,6 +64,12 @@ export default {
         this.$router.push({ name: 'Documents', params: { folderId: parentFolder.id } })
       } else {
         console.error('cannot go in parent folder of a folder with index ' + index)
+      }
+    },
+    changeDir (item) {
+      if (!this.isCurrentFolder) {
+        this.$router.push({ name: 'Documents', params: { folderId: item.id } })
+        // this.$store.dispatch('documents/closeDocumentPanel') // TODO: discuss about ergonomics
       }
     }
   }
