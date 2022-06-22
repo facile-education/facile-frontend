01e5d4b407f70234e80712bb8824d1d1b8b76dda
vdona <vdona@pentila.com>
2022-06-22 10:45:40 +0200

    Display context menu on hiddenOptions hover

--- src/components/Base/HiddenItems.vue
+++ src/components/Base/HiddenItems.vue
@@ -2,18 +2,22 @@
   <div
     ref="hiddenItems"
     class="container"
+    :class="{ 'active': isActive}"
     @click="handleClick"
+    @dragenter="setActive"
+    @dragleave="cancelActive"
+    @drop="dropFile"
   >
     <div class="dots">
       ...
     </div>
-  </div>
 
-  <ContextMenu
-    v-if="isContextMenuDisplayed && isAContextMenuDisplayed"
-    @chooseOption="subMenuOptionClicked"
-    @close="isContextMenuDisplayed=false"
-  />
+    <ContextMenu
+      v-if="isContextMenuDisplayed && isAContextMenuDisplayed"
+      @chooseOption="subMenuOptionClicked"
+      @close="isContextMenuDisplayed=false"
+    />
+  </div>
 </template>
 
 <script>
@@ -26,20 +30,57 @@ export default {
     items: {
       type: Array,
       required: true
+    },
+    isHoverable: {
+      type: Boolean,
+      default: false
     }
   },
   emits: ['optionClicked'],
   data () {
     return {
-      isContextMenuDisplayed: false
+      isContextMenuDisplayed: false,
+      hoveringElementCount: 0
     }
   },
   computed: {
+    isActive () {
+      return this.hoveringElementCount > 0
+    },
     isAContextMenuDisplayed () {
       return this.$store.state.contextMenu.isAContextMenuDisplayed
+    },
+    isThereInternDocumentDrag () {
+      return this.$store.state.misc.isThereDocumentDrag
     }
   },
   methods: {
+    cancelHandlers (e) {
+      e.preventDefault()
+      e.stopPropagation()
+    },
+    setActive (e) {
+      if (this.isThereInternDocumentDrag && this.isHoverable) {
+        this.hoveringElementCount++
+        this.openSubMenu(e)
+        this.cancelHandlers(e)
+      }
+    },
+    cancelActive (e) {
+      if (this.isThereInternDocumentDrag && this.isHoverable) {
+        this.hoveringElementCount--
+        if (!this.isActive) {
+          this.$store.dispatch('contextMenu/closeMenus')
+          this.isContextMenuDisplayed = false
+        }
+        this.cancelHandlers(e)
+      }
+    },
+    dropFile (e) {
+      if (this.isThereInternDocumentDrag) {
+        this.cancelActive(e)
+      }
+    },
     handleClick (event) {
       this.openSubMenu(event)
     },
@@ -63,17 +104,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@design";
 
 .container {
   width: 50px;
+  height: 100%;
+  border-radius: 6px;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
 
   .dots {
-
     font-size: 20px;
+  }
+
+  &.active {
+    background-color: $color-active-bg;
   }
 }
 
