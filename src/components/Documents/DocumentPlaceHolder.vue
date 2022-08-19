6084107fde1b06f066c5b30cd0ac731976f04d00
vdona <vdona@pentila.com>
2022-08-19 09:46:44 +0200

    [Documents] Review PlaceHolder

--- src/components/Documents/DocumentPlaceHolder.vue
+++ src/components/Documents/DocumentPlaceHolder.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="placeholder">
     <div class="message">
-      <BaseIcon name="folder" />
-      <p v-t="'noDocumentToDisplay'" />
+      <div class="icon-container">
+        <img
+          class="icon"
+          src="@/assets/options/icon_fichier_ajout.svg"
+          alt=""
+        >
+      </div>
+      <p> {{ text }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import BaseIcon from '@components/Base/BaseIcon'
 export default {
   name: 'DocumentPlaceHolder',
-  components: { BaseIcon }
+  inject: ['mq'],
+  computed: {
+    text () {
+      if (this.mq.phone || this.mq.tablet) {
+        return this.$t('noDocumentToDisplayMobile')
+      } else {
+        return this.$t('noDocumentToDisplay')
+      }
+    }
+  }
 }
 </script>
 
@@ -19,28 +33,45 @@ export default {
 @import '@design';
 
 .placeholder{
-  height: 100%;
+  height: calc(100% - #{$doc-breadcrumb-size});
   width: 100%;
   margin-right: auto;
   margin-left: auto;
   padding-top: 5em;
-  font-size: 1.75em;
+  font-size: 1.25em;
 }
 
 .message {
   display: flex;
   justify-content: center;
   align-items: center;
-}
+  flex-direction: column;
 
-p{
-  padding-left: 20px;
+  .icon-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 97px;
+    height: 97px;
+    border-radius: 48px;
+    background-color: #F3F6F8;
+
+    .icon {
+      margin-left: 9px;
+    }
+  }
+
+  p{
+    font-size: 14px;
+    font-weight: 600;
+  }
 }
 
 </style>
 
 <i18n locale="fr">
 {
-  "noDocumentToDisplay": "Ajoutez un document avec le bouton + ou bien par glisser-déposer"
+  "noDocumentToDisplay": "Glisser / déposer un document",
+  "noDocumentToDisplayMobile": "Ajouter un document avec le bouton +"
 }
 </i18n>
