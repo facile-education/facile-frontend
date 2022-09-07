75aab5b5237c4baadc2edb3a8ebde043b56cd700
vdona <vdona@pentila.com>
2022-09-07 05:43:40 +0200

    [Documents / Groups] Permissions modale

---
+++ src/components/Documents/Modals/PermissionModal/PermissionsModal.vue
@@ -0,0 +1,194 @@
+<template>
+  <PentilaWindow
+    :full-screen="mq.phone"
+    :modal="true"
+    :draggable="true"
+    :width="700"
+    :height="600"
+    @close="onClose"
+    @keydown.exact.enter.stop=""
+    @keydown.exact.backspace.stop=""
+    @keydown.exact.delete.stop=""
+    @keydown.exact.f2.stop=""
+    @keydown.ctrl.stop=""
+  >
+    <template #header>
+      <span v-t="'header'" />
+    </template>
+
+    <template #body>
+      <div class="fields">
+        <div
+          v-t="('name')"
+          class="name"
+        />
+        <div
+          v-t="('view')"
+          class="view"
+        />
+        <div
+          v-if="document.type==='File'"
+          v-t="('update')"
+          class="update"
+        />
+        <div
+          v-if="document.type==='Folder'"
+          v-t="('add')"
+          class="add"
+        />
+        <div
+          v-t="('delete')"
+          class="delete"
+        />
+        <div
+          v-t="('permissions')"
+          class="permissions"
+        />
+      </div>
+      <RolePermissions
+        v-for="(role, index) in permissionMatrix"
+        :key="index"
+        :role="role"
+        :type="document.type"
+        @updateMatrix="updateMatrix($event, role)"
+      />
+      <PentilaCheckbox
+        v-if="document.type==='Folder'"
+        :model-value="isRecursive"
+        :label="$t('recursiveLabel')"
+        @update:modelValue="updateRecursive"
+      />
+    </template>
+
+    <template #footer>
+      <PentilaButton
+        v-if="initialPermissionMatrix.length > 0"
+        class="reset-button"
+        :label="$t('reset')"
+        @click="reset"
+      />
+      <PentilaButton
+        data-test="submitButton"
+        :label="$t('Commons.submit')"
+        @click="submit"
+      />
+    </template>
+  </PentilaWindow>
+</template>
+
+<script>
+import permissionsService from '@/api/documents/permissions.service'
+import RolePermissions from '@components/Documents/Modals/PermissionModal/RolePermissions'
+
+export default {
+  name: 'PermissionsModal',
+  components: { RolePermissions },
+  inject: ['mq'],
+  props: {
+    document: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ['close'],
+  data () {
+    return {
+      initialPermissionMatrix: [],
+      permissionMatrix: [],
+      isRecursive: false
+    }
+  },
+  created () {
+    if (this.document.type === 'File') {
+      permissionsService.getFilePermissionMatrix(this.document.id).then((data) => {
+        if (data.success) {
+          this.initialPermissionMatrix = [...data.permissionMatrix]
+          this.permissionMatrix = data.permissionMatrix
+        }
+      })
+    } else if (this.document.type === 'Folder') {
+      permissionsService.getFolderPermissionMatrix(this.document.id).then((data) => {
+        if (data.success) {
+          this.initialPermissionMatrix = [...data.permissionMatrix]
+          this.permissionMatrix = data.permissionMatrix
+        }
+      })
+    } else {
+      console.error('Unknown document type' + this.document.type)
+    }
+  },
+  methods: {
+    updateMatrix (action, role) {
+      for (let i = 0; i < this.permissionMatrix.length; i++) {
+        // console.log(role)
+        const row = this.permissionMatrix[i]
+        if (row.roleId === role.roleId) {
+          row[action.actionName] = action.value ? 'true' : 'false'
+        }
+      }
+    },
+    updateRecursive (value) {
+      this.isRecursive = value
+    },
+    reset () {
+      this.permissionMatrix = [...this.initialPermissionMatrix]
+    },
+    submit () {
+      if (this.document.type === 'File') {
+        permissionsService.saveFilePermissionMatrix(this.document.id, this.permissionMatrix, this.isRecursive).then((data) => {
+          if (data.success) {
+            this.onClose()
+          }
+        })
+      } else if (this.document.type === 'Folder') {
+        permissionsService.saveFolderPermissionMatrix(this.document.id, this.permissionMatrix).then((data) => {
+          if (data.success) {
+            this.onClose()
+          }
+        })
+      }
+    },
+    onClose () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.fields {
+  display: flex;
+  height: 25px;
+  font-weight: 500;
+
+  .name {
+    width: 250px;
+  }
+
+  .add, .view, .update, .delete, .permissions {
+    display: flex;
+    justify-content: center;
+    width: 100px;
+  }
+}
+
+.reset-button {
+  margin-right: 20px;
+}
+
+</style>
+
+<i18n locale="fr">
+{
+  "header": "Gestion des permissions",
+  "reset": "Réinitialiser",
+  "add": "Ajouter",
+  "view": "Voir",
+  "update": "Modifier",
+  "delete": "Supprimer",
+  "permissions": "Permissions",
+  "recursiveLabel": "Appliquer ces permissions à tous les éléments contenus dans le dossier",
+  "name": "Role"
+}
+</i18n>
---
+++ src/components/Documents/Modals/PermissionModal/RolePermissions.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="role">
+    <div class="role-name">
+      {{ role.roleName }}
+    </div>
+
+    <div class="permission view">
+      <PentilaCheckbox
+        v-if="role.VIEW !== undefined"
+        :model-value="role.VIEW === 'true'"
+        :disabled="!role.editable"
+        :label="''"
+        @update:modelValue="updateMatrix($event, 'VIEW')"
+      />
+    </div>
+
+    <div
+      v-if="type==='Folder'"
+      class="permission add"
+    >
+      <PentilaCheckbox
+        v-if="role.ADD_OBJECT !== undefined"
+        :model-value="role.ADD_OBJECT === 'true'"
+        :disabled="!role.editable"
+        :label="''"
+        @update:modelValue="updateMatrix($event, 'ADD_OBJECT')"
+      />
+    </div>
+
+    <div
+      v-if="type==='File'"
+      class="permission update"
+    >
+      <PentilaCheckbox
+        v-if="role.UPDATE !== undefined"
+        :model-value="role.UPDATE === 'true'"
+        :disabled="!role.editable"
+        :label="''"
+        @update:modelValue="updateMatrix($event, 'UPDATE')"
+      />
+    </div>
+
+    <div class="permission delete">
+      <PentilaCheckbox
+        v-if="role.DELETE !== undefined"
+        :model-value="role.DELETE === 'true'"
+        :disabled="!role.editable"
+        :label="''"
+        @update:modelValue="updateMatrix($event, 'DELETE')"
+      />
+    </div>
+
+    <div class="permission permissions">
+      <PentilaCheckbox
+        v-if="role.PERMISSIONS !== undefined"
+        :model-value="role.PERMISSIONS === 'true'"
+        :disabled="!role.editable"
+        :label="''"
+        @update:modelValue="updateMatrix($event, 'PERMISSIONS')"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RolePermissions',
+  props: {
+    role: {
+      type: Object,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['updateMatrix'],
+  methods: {
+    updateMatrix (event, action) {
+      this.$emit('updateMatrix', { actionName: action, value: event })
+      if (action === 'VIEW' && !event) { // Unselect other permissions if the view permission is set to "false"
+        this.$emit('updateMatrix', { actionName: 'DELETE', value: false })
+        this.$emit('updateMatrix', { actionName: 'PERMISSIONS', value: false })
+        this.type === 'Folder' ? this.$emit('updateMatrix', { actionName: 'ADD_OBJECT', value: false }) : this.$emit('updateMatrix', { actionName: 'UPDATE', value: false })
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.role {
+  height: 25px;
+  display: flex;
+
+  .role-name {
+    width: 250px;
+  }
+
+  .permission {
+    display: flex;
+    justify-content: center;
+    width: 100px;
+  }
+}
+</style>
