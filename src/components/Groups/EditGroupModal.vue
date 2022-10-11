c854a57febbb34353a55a41553780038398d2b25
Cedric <cedric.lecarpentier@weprode.com>
2022-10-11 11:43:30 +0200

    #4039 - Init new groups service

---
+++ src/components/Groups/EditGroupModal.vue
@@ -0,0 +1,163 @@
+<template>
+  <PentilaWindow
+    :modal="true"
+    data-test="edit-group-modal"
+    class="editWindow"
+    @close="closeModal"
+  >
+    <template #header>
+      <span v-t="'group'" />
+    </template>
+
+    <template #body>
+      <form id="groupform">
+        <div class="group-name">
+          <PentilaInput
+            ref="name"
+            v-model="group.name"
+            :placeholder="$t('title')"
+            maxlength="75"
+            @blur="v$.group.name.$touch()"
+          />
+          <PentilaErrorMessage :error-message="formErrorList.groupName" />
+          <ColorPicker v-model="progression.color" />
+        </div>
+        <PentilaTextArea
+          v-model="group.description"
+          :placeholder="$t('description')"
+          maxlength="75"
+          style="height: 120px;"
+          class="description"
+        />
+      </form>
+    </template>
+
+    <template #footer>
+      <PentilaButton
+        form="groupform"
+        :label="buttonLabel"
+        class="confirm-button"
+        :class="{'disabled' : v$.$invalid}"
+        @click="onConfirm"
+      />
+    </template>
+  </PentilaWindow>
+</template>
+
+<script>
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+import { nextTick } from 'vue'
+import PentilaUtils from 'pentila-utils'
+import ColorPicker from '@/components/Nero/ColorPicker'
+
+export default {
+  name: 'EditGroupModal',
+  components: { ColorPicker },
+  inject: ['mq'],
+  props: {
+    editedGroup: {
+      type: Object,
+      default: undefined
+    }
+  },
+  emits: ['close'],
+  setup: () => ({ v$: useVuelidate() }),
+  validations: {
+    group: {
+      name: { required }
+    }
+  },
+  data () {
+    return {
+      group: {
+        name: '',
+        color: '',
+        subject: undefined,
+        subjectId: 0,
+        volee: undefined,
+        description: ''
+      }
+    }
+  },
+  computed: {
+    formErrorList () {
+      const form = this.v$.progression
+      return {
+        name: (form.name.$invalid && form.name.$dirty) ? this.$t('Commons.required') : ''
+      }
+    },
+    buttonLabel () {
+      if (this.editedGroup !== undefined) {
+        return this.$t('edit')
+      } else {
+        return this.$t('create')
+      }
+    }
+  },
+  created () {
+    if (this.editedGroup !== undefined) {
+      this.group = PentilaUtils.JSON.deepCopy(this.editedGroup)
+    }
+    nextTick(() => this.$refs.title.$el.childNodes[0].focus())
+  },
+  methods: {
+    closeModal () {
+      this.$emit('close')
+    },
+    onConfirm (e) {
+      e.preventDefault()
+      if (this.v$.$invalid) {
+        this.v$.$touch()
+      } else {
+        this.submitChanges()
+      }
+    },
+    submitChanges () {
+      if (this.group.groupId > 0) {
+        // Update
+        this.$store.dispatch('groups/updateGroup', this.group)
+      } else {
+        // Creation
+        this.$store.dispatch('groups/createGroup', this.group)
+      }
+      this.closeModal()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.title-color {
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+
+  .group {
+    flex-grow: 1;
+    margin-right: 10px;
+  }
+
+  .error-message {
+    position: absolute;
+    top: 46px;
+  }
+}
+
+.description {
+  width: 100%;
+}
+
+.confirm-button {
+  width: 130px;
+}
+</style>
+
+<i18n locale="fr">
+{
+  "create": "Créer",
+  "edit": "Modifier",
+  "description": "Description",
+  "name": "Nom du groupe"
+}
+</i18n>
---
+++ src/components/Groups/GroupItem.vue
@@ -0,0 +1,111 @@
+<template>
+  <div
+    data-test="group-item"
+    class="group"
+    @click="selectGroup()"
+  >
+    <div
+      class="header"
+      :style="`background-color:${group.color};`"
+    >
+      <h3>{{ group.name }}</h3>
+    </div>
+    <div class="buttons">
+      <img
+        class="button"
+        src="@assets/edit.svg"
+        data-test="edit-group-icon"
+        :alt="$t('edit')"
+        :title="$t('edit')"
+        @click.stop="editGroup()"
+      >
+      <img
+        class="button"
+        src="@assets/trash.svg"
+        data-test="delete-group-icon"
+        :alt="$t('delete')"
+        :title="$t('delete')"
+        @click.stop="confirmGroupDeletion()"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroupItem',
+  props: {
+    group: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ['edit'],
+  methods: {
+    confirmGroupDeletion () {
+      this.$store.dispatch('warningModal/addWarning', {
+        text: this.$t('warning'),
+        lastAction: { fct: this.deleteGroup, params: [this.group] }
+      })
+    },
+    deleteGroup () {
+      this.$store.dispatch('groups/deleteGroup', this.group)
+    },
+    editGroup () {
+      this.$emit('edit', this.groupœ)
+    },
+    selectGroup () {
+      this.$router.push({ name: 'Group', params: { groupId: this.group.groupId } })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@design';
+
+.group {
+  display: flex;
+  flex-direction: column;
+  height: 235px;
+  max-height: 235px;
+  width: 235px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    border: 1px solid grey;
+    cursor: pointer;
+  }
+}
+
+.body {
+  padding: 10px 15px;
+}
+
+.buttons {
+  margin-top: auto;
+  padding: 0 15px;
+  text-align: right;
+
+  .button {
+    border: 1px solid transparent;
+    border-radius: 5px;
+    padding: 5px;
+    margin: 0 0 5px 5px;
+
+    &:hover {
+      border: 1px solid grey;
+      cursor: pointer;
+    }
+  }
+}
+</style>
+
+<i18n locale="fr">
+{
+  "warning": "La suppression de ce groupe est définitive.",
+  "edit": "Modifier",
+  "delete": "Supprimer"
+}
+</i18n>
