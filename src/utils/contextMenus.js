4b4c262e30e25d8c6fb1f7eb2e6f200f0f1d02c1
vdona <vdona@pentila.com>
2022-10-28 07:32:10 +0200

    [Messaging] Fix messaging context menu

--- src/utils/contextMenus.js
+++ src/utils/contextMenus.js
@@ -7,7 +7,6 @@ const contextMenus = {
       name: 'reply',
       title: i18n.global.t('Messaging.reply'),
       icon: icons.options.reply,
-      iconWhite: icons.options.reply_white,
       position: 2,
       hasSeparator: false
     },
@@ -15,7 +14,6 @@ const contextMenus = {
       name: 'replyAll',
       title: i18n.global.t('Messaging.replyAll'),
       icon: icons.options.replyAll,
-      iconWhite: icons.options.replyAll_white,
       position: 3,
       hasSeparator: false
     },
@@ -23,7 +21,6 @@ const contextMenus = {
       name: 'forward',
       title: i18n.global.t('Messaging.forward'),
       icon: icons.options.forward,
-      iconWhite: icons.options.forward_white,
       position: 4,
       hasSeparator: false
     },
@@ -31,7 +28,6 @@ const contextMenus = {
       name: 'delete',
       title: i18n.global.t('Messaging.deleteMessage'),
       icon: icons.options.delete,
-      iconWhite: icons.options.delete_white,
       iconSize: '25px',
       position: 5,
       hasSeparator: false
@@ -51,7 +47,6 @@ const contextMenus = {
       name: 'markAsRead',
       title: i18n.global.t('Messaging.markAsRead'),
       icon: icons.options.markAsRead,
-      iconWhite: icons.options.markAsRead_white,
       position: 0,
       hasSeparator: false
     }
@@ -61,7 +56,6 @@ const contextMenus = {
       name: 'editDraft',
       title: i18n.global.t('Messaging.editDraft'),
       icon: icons.options.modify,
-      iconWhite: icons.options.modify_white,
       position: 0,
       hasSeparator: false
     }
@@ -71,7 +65,6 @@ const contextMenus = {
       name: 'markAsUnread',
       title: i18n.global.t('Messaging.markAsUnread'),
       icon: icons.options.markAsRead,
-      iconWhite: icons.options.markAsRead_white,
       position: 0,
       hasSeparator: false
     }
