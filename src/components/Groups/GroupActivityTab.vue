ed5409809b0ddcc7eabc9230b097013cd2b1f6e8
vdona <vdona@pentila.com>
2022-11-04 09:27:32 +0100

    [Groups] Set tabs in group Details

---
+++ src/components/Groups/GroupActivityTab.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="details-tab">
+    <div class="expiration-date">
+      {{ $t('expire') + formattedDate}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroupActivityTab',
+  props: {
+    group: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
