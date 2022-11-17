b8198aef742b7a384ba0a3a3a70d31298358cb55
vdona <vdona@pentila.com>
2022-11-17 07:49:35 +0100

    [Preferences] Select correct tab on preferenceModal Creation

--- src/components/Preferences/PreferencesModal.vue
+++ src/components/Preferences/PreferencesModal.vue
@@ -1,35 +1,20 @@
 <template>
   <PentilaWindow
     :modal="true"
-    @close="close()"
+    @close="close"
   >
     <template #header>
       <span v-t="'modalHeaderLabel'" />
     </template>
 
     <template #body>
-      <PentilaTabList>
+      <PentilaTabList ref="tabList">
         <PentilaTabItem :title="$t('accountTabLabel')">
           <AccountTab />
         </PentilaTabItem>
         <PentilaTabItem :title="$t('messagingTabLabel')">
           <MessagingTab />
         </PentilaTabItem>
-        <!--        <PentilaTabItem :title="$t('accountTabLabel')">-->
-        <!--          <PersonalDetails />-->
-        <!--        </PentilaTabItem>-->
-        <!--        <PentilaTabItem :title="$t('interfaceTabLabel')">-->
-        <!--          <InterfaceDetails />-->
-        <!--        </PentilaTabItem>-->
-        <!--        <PentilaTabItem :title="$t('notificationsTabLabel')">-->
-        <!--          <NotificationDetails />-->
-        <!--        </PentilaTabItem>-->
-        <!--        <PentilaTabItem :title="$t('backupTabLabel')">-->
-        <!--          <BackupDetails />-->
-        <!--        </PentilaTabItem>-->
-        <!--        <PentilaTabItem :title="$t('webdavTabLabel')">-->
-        <!--          <WebdavDetails />-->
-        <!--        </PentilaTabItem>-->
       </PentilaTabList>
     </template>
   </PentilaWindow>
@@ -39,33 +24,33 @@
 import { defineAsyncComponent } from 'vue'
 const AccountTab = defineAsyncComponent(() => import('@/components/Preferences/AccountTab'))
 const MessagingTab = defineAsyncComponent(() => import('@/components/Preferences/MessagingTab'))
-// import PersonalDetails from '@/components/Preferences/PersonalDetails'
-// const BackupDetails = defineAsyncComponent(() => import('@/components/Preferences/BackupDetails'))
-// const InterfaceDetails = defineAsyncComponent(() => import('@/components/Preferences/InterfaceDetails'))
-// const NotificationDetails = defineAsyncComponent(() => import('@/components/Preferences/NotificationDetails'))
-// const WebdavDetails = defineAsyncComponent(() => import('@/components/Preferences/WebdavDetails'))
 
 export default {
   name: 'PreferencesModal',
   components: {
     MessagingTab,
     AccountTab
-    // BackupDetails,
-    // InterfaceDetails,
-    // NotificationDetails,
-    // PersonalDetails,
-    // WebdavDetails
   },
   props: {
     tab: {
       type: String,
-      default: 'parameters'
+      default: 'account'
     }
   },
   created () {
     this.$store.dispatch('misc/incrementModalCount')
   },
+  mounted () {
+    this.selectTab(this.tab)
+  },
   methods: {
+    selectTab (tabName) {
+      if (tabName === 'account') {
+        this.$refs.tabList.selectTab(0)
+      } else {
+        this.$refs.tabList.selectTab(1)
+      }
+    },
     close () {
       this.$store.dispatch('misc/decreaseModalCount')
       this.$store.dispatch('messaging/closeParametersModal')
