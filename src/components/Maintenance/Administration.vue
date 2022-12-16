9d77ab654a15f348e1b7b1a1b0fc3b78ee6ef327
vdona <vdona@pentila.com>
2022-12-16 11:31:21 +0100

    [ent-Tools] Add missing interface

--- src/components/Maintenance/Administration.vue
+++ src/components/Maintenance/Administration.vue
@@ -6,12 +6,24 @@
     >
       <span>{{ $t('message-migration') }}</span>
     </PentilaButton>
+    <PentilaButton
+      class="round"
+      @click="runSynchro"
+    >
+      <span>{{ $t('synchro') }}</span>
+    </PentilaButton>
+    <PentilaButton
+      class="round"
+      @click="runParentSynchro"
+    >
+      <span>{{ $t('parentSynchro') }}</span>
+    </PentilaButton>
   </div>
 </template>
 
 <script>
 
-import { runMessageMigration } from '@/api/maintenance.service'
+import { runMessageMigration, startSynchro, startParentSynchro } from '@/api/maintenance.service'
 
 export default {
   name: 'Administration',
@@ -34,18 +46,46 @@ export default {
           }
         }
       )
+    },
+    runSynchro () {
+      startSynchro().then(
+        (data) => {
+          if (data.success) {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('success'), type: 'success' })
+          } else {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
+          }
+        }
+      )
+    },
+    runParentSynchro () {
+      startParentSynchro().then(
+        (data) => {
+          if (data.success) {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('success'), type: 'success' })
+          } else {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
+          }
+        }
+      )
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.param-header {
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+}
 </style>
 
 <i18n locale="fr">
 {
   "message-migration": "Migration des messages",
+  "synchro": "Synchro",
+  "parentSynchro": "Synchro Parents",
   "success": "Migration terminée en succès",
   "error": "Migration terminée en erreur"
 }
--- src/components/Maintenance/GroupsMaintenance.vue
+++ src/components/Maintenance/GroupsMaintenance.vue
@@ -6,17 +6,37 @@
     >
       <span>{{ $t('archive') }}</span>
     </PentilaButton>
+
+    <PentilaInput
+      v-model="groupId"
+      placeholder="group id"
+      :maxlength="75"
+    />
+
+    <input
+      type="file"
+      name="file"
+      @change="fileChange($event.target.files)"
+    >
+    <button
+      type="button"
+      @click="deleteGroups"
+    >
+      Upload
+    </button>
   </div>
 </template>
 
 <script>
 
-import runArchiving from '@/api/maintenance.service'
+import { runArchiving, deleteGroup, deleteGroups } from '@/api/maintenance.service'
 
 export default {
   name: 'GroupsMaintenance',
   data () {
     return {
+      groupId: undefined,
+      files: []
     }
   },
   computed: {
@@ -24,8 +44,34 @@ export default {
   created () {
   },
   methods: {
+    fileChange (fileList) {
+      this.files = new FormData()
+      this.files.append('file', fileList[0], fileList[0].name)
+    },
     runArchiving () {
       runArchiving().then(
+        (data) => {
+          if (data.success) {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('success'), type: 'success' })
+          } else {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
+          }
+        }
+      )
+    },
+    deleteGroup () {
+      deleteGroup(this.groupId).then(
+        (data) => {
+          if (data.success) {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('success'), type: 'success' })
+          } else {
+            this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
+          }
+        }
+      )
+    },
+    deleteGroups () {
+      deleteGroups(this.files[0]).then(
         (data) => {
           if (data.success) {
             this.$store.dispatch('popups/pushPopup', { message: this.$t('success'), type: 'success' })
