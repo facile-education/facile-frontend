9640e2bd54f59c9bc1c03bb4ce762f3d021f499c
vdona <vdona@pentila.com>
2022-12-16 11:31:21 +0100

    [ent-Tools] Add missing interface

--- src/api/maintenance.service.js
+++ src/api/maintenance.service.js
@@ -1,22 +1,58 @@
 import axios from 'axios'
 import constants from '@/api/constants'
+import PentilaUtils from 'pentila-utils'
+import { getCookie } from '@utils/browser.util'
 
 export {
   runMessageMigration,
-  runArchiving
+  startSynchro,
+  startParentSynchro,
+  runArchiving,
+  deleteGroup,
+  deleteGroups
 }
 
 export default {
   runMessageMigration,
-  runArchiving
+  startSynchro,
+  startParentSynchro,
+  runArchiving,
+  deleteGroup,
+  deleteGroups
 }
 
 const MAINTENANCE_PATH = '/entTools-portlet.maintenance'
+const GROUPS_PATH = '/entTools-portlet.groupsMaintenance'
 
 function runMessageMigration () {
   return axios.get(constants.JSON_WS_URL + MAINTENANCE_PATH + '/run-message-migration').then(response => response.data)
 }
 
+function startSynchro () {
+  return axios.post(constants.JSON_WS_URL + MAINTENANCE_PATH + '/start-synchro').then(response => response.data)
+}
+function startParentSynchro () {
+  return axios.post(constants.JSON_WS_URL + MAINTENANCE_PATH + '/start-parent-synchro').then(response => response.data)
+}
+
 function runArchiving () {
-  return axios.get(constants.JSON_WS_URL + MAINTENANCE_PATH + '/archive').then(response => response.data)
+  return axios.get(constants.JSON_WS_URL + GROUPS_PATH + '/archive-groups').then(response => response.data)
+}
+
+function deleteGroup (groupId) {
+  return axios.delete(constants.JSON_WS_URL + GROUPS_PATH + '/delete-groups',
+    PentilaUtils.URL.params({
+      groupId
+    }).then(response => response.data))
+}
+
+function deleteGroups (file) {
+  const formData = new FormData()
+  formData.append('p_auth', getCookie('pauth'))
+  formData.append('file', file)
+
+  return axios.post(
+    constants.JSON_WS_URL + GROUPS_PATH + '/delete-groups?',
+    formData
+  ).then(response => response.data)
 }
