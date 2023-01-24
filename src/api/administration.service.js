b9930784f3c33e5def93fd00268aa4acf1fc880f
Marc Salvat <marc.salvat@weprode.com>
2023-01-24 11:25:15 +0100

    Update get schools WS calls.

--- src/api/administration.service.js
+++ src/api/administration.service.js
@@ -2,19 +2,11 @@ import axios from 'axios'
 import constants from './constants'
 
 export default {
-  getAdministeredSchoolList,
   getPortletList
 }
 
 const SERVICEMANAGER_PATH = '/gestionApplications-portlet.'
 const SERVICE_CTX = '/'
-
-function getAdministeredSchoolList () {
-  return axios.get(constants.JSON_WS_URL + SERVICEMANAGER_PATH + SERVICE_CTX + 'get-schools', {
-    params: {
-    }
-  }).then(response => response.data)
-}
 
 function getPortletList () {
   return axios.get(constants.JSON_WS_URL + SERVICEMANAGER_PATH + SERVICE_CTX + 'get-portlets', {
