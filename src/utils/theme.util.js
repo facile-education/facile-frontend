e1df939d5fb178fa52504fcac8727adfdaaf8722
vdona <vdona@pentila.com>
2023-03-21 16:46:20 +0100

    [Messaging] Add new opacity level of theme color, and use it in messaging thread

--- src/utils/theme.util.js
+++ src/utils/theme.util.js
@@ -5,7 +5,8 @@ const fontColor = '#fontColor'
 // --vdp : Vue Date picker color variables
 const style = `.theme-text-color { color: ${mainColor}; }
     .theme-background-color { color: ${fontColor}; background-color: ${mainColor}; }
-    .theme-light-background-color { background-color: ${mainColor}27; }
+    .theme-light-background-color { background-color: ${mainColor}30; }
+    .theme-extra-light-background-color { background-color: ${mainColor}10; }
     .theme-hover-background-color:hover { color: ${fontColor}; background-color: ${mainColor}; }
     .theme-hover-light-background-color:hover { background-color: ${mainColor}27; }
     .theme-hover-text-color:hover { color: ${mainColor}; }
