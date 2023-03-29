e0762e351f82f8f5dd9be364497a2b0385bc1bb0
vdona <vdona@pentila.com>
2023-03-29 18:30:13 +0200

    [Messaging] Sort personal folders

--- src/components/Messaging/Menu.vue
+++ src/components/Messaging/Menu.vue
@@ -126,6 +126,7 @@ import MenuFolder from '@components/Messaging/MenuFolder'
 import MenuRootFolder from '@components/Messaging/MenuRootFolder'
 import { nextTick } from 'vue'
 import messagingUtils from '@utils/messaging.utils'
+import PentilaUtils from 'pentila-utils'
 
 export default {
   name: 'Menu',
@@ -167,7 +168,7 @@ export default {
       return this.folderList.find(folder => folder.type === constants.messagingTrashFolderType)
     },
     personalFolders () {
-      return this.folderList.filter(folder => folder.type === constants.messagingPersonalFolderType)
+      return PentilaUtils.Array.sortWithString(this.folderList.filter(folder => folder.type === constants.messagingPersonalFolderType), false, 'folderName')
     },
     isAPersonalFolderSelected () {
       for (let i = 0; i < this.personalFolders.length; i++) {
