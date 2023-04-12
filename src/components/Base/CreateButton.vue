00c543cc3950140af0622de4d5fac1c92bf56348
vdona <vdona@pentila.com>
2023-04-12 15:47:03 +0200

    [Messaging] Revert to previous pentila components + hope fix create Button

--- src/components/Base/CreateButton.vue
+++ src/components/Base/CreateButton.vue
@@ -13,9 +13,10 @@ export default {
 <style lang="scss" scoped>
 button {
   --size: 33px;
-  height: 33px;
-  width: 33px;
+  height: var(--size);
+  width: var(--size);
   border-radius: 50%;
+  padding: 0;
   color: white;
   display: flex;
   align-items: center;
