dd189118fbceafe8bd4116a714351b1babfd7c3f
vdona <vdona@pentila.com>
2023-04-17 14:44:07 +0200

    [Dashboard] Vertical transition in userReadInfo lists

--- src/components/Dashboard/ReadInfos/ReadInfoModalPopulation.vue
+++ src/components/Dashboard/ReadInfos/ReadInfoModalPopulation.vue
@@ -17,15 +17,17 @@
     </span>
   </button>
 
-  <ul v-if="isExtended">
-    <!-- TODO: use userId as key-->
-    <li
-      v-for="(member, index) in sortedMembers"
-      :key="index"
-    >
-      <ReadInfoModalUser :user="member" />
-    </li>
-  </ul>
+  <Transition name="slide-fade">
+    <ul v-if="isExtended">
+      <!-- TODO: use userId as key-->
+      <li
+        v-for="(member, index) in sortedMembers"
+        :key="index"
+      >
+        <ReadInfoModalUser :user="member" />
+      </li>
+    </ul>
+  </Transition>
 </template>
 
 <script>
@@ -73,6 +75,7 @@ ul {
   margin: 0;
   padding: 0;
   list-style-type: none;
+  overflow-y: hidden;
 }
 
 .population {
@@ -86,7 +89,7 @@ ul {
 }
 
 .icon-collapse, .icon-extend {
-  transition:  transform .6s;
+  transition:  transform .4s;
   margin-left: 1rem;
 }
 
@@ -96,5 +99,20 @@ ul {
 
 .icon-collapse {
   transform: rotate(90deg);
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease-in;
+}
+.slide-fade-leave-active {
+  transition: all .3s ease-out;
+}
+
+.slide-fade-enter-from, .slide-fade-leave-to {
+  max-height: 0;
+}
+.slide-fade-enter-to, .slide-fade-leave-from {
+  /* TODO: make max-height adaptive to content height */
+  max-height: 300px;
 }
 </style>
