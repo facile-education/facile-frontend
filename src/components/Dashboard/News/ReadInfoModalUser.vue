8cf4b668f1bf44c04cdb425fd2ee8c4e177b78ad
vdona <vdona@pentila.com>
2023-04-17 10:35:30 +0200

    [Dashboard] Create ReadInfo modal

---
+++ src/components/Dashboard/News/ReadInfoModalUser.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="read-info-user">
+    <div class="userName">
+      {{ user.fullName }}
+    </div>
+    <div class="readStatus">
+      {{ user.hasRead ? $t('read'): $t('unread') }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReadInfoModalUser',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.read-info-user {
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 2rem;
+}
+</style>
+
+<i18n locale="fr" >
+{
+  "read": "Lu",
+  "unread": "Non lu"
+}
+</i18n>
