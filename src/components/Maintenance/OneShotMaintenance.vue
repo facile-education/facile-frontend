29bd15c1c43319b83f8dd3cadb1d7413eb245687
Cedric <cedric.lecarpentier@weprode.com>
2023-04-25 21:47:31 +0200

    #4213 - Maintenance service migration

--- src/components/Maintenance/OneShotMaintenance.vue
+++ src/components/Maintenance/OneShotMaintenance.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="param-header">
-    <PentilaButton
-      class="round"
-      @click="runMessageMigration"
-    >
-      <span>{{ $t('message-migration') }}</span>
-    </PentilaButton>
+    <!-- add tool here -->
   </div>
 </template>
 
 <script>
-
-import { runMessageMigration } from '@/api/maintenance.service'
 
 export default {
   name: 'OneShotMaintenance',
@@ -24,17 +17,6 @@ export default {
   created () {
   },
   methods: {
-    runMessageMigration () {
-      runMessageMigration().then(
-        (data) => {
-          if (data.success) {
-            this.$store.dispatch('popups/pushPopup', { message: this.$t('success'), type: 'success' })
-          } else {
-            this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
-          }
-        }
-      )
-    }
   }
 }
 </script>
