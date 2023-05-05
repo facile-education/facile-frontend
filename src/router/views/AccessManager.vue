673677976c5c5e8eb3af761a36dff348f946e397
Cedric <cedric.lecarpentier@weprode.com>
2023-05-05 18:46:56 +0200

    #4055 - Init access manager services

---
+++ src/router/views/AccessManager.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout
+    :is-allowed="!$store.state.user.isStudent && !$store.state.user.isParent"
+    class="layout"
+  >
+    <h1 :aria-label="$t('serviceTitle')" />
+    <p>Gestion des accès TODO</p>
+  </Layout>
+</template>
+
+<script>
+import Layout from '@/router/layouts/BannerLayout'
+
+export default {
+  name: 'AccessManager',
+  components: {
+    Layout
+  },
+  computed: {
+  },
+  created () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  height: 100%;
+}
+</style>
+
+<i18n locale="fr">
+{
+  "serviceTitle": "Gestion des accès"
+}
+</i18n>
