3385db8003a02ee64a7de2d7e4e2fe670003cd1b
Cedric <cedric.lecarpentier@weprode.com>
2023-05-05 18:46:56 +0200

    #4055 - Init access manager services

---
+++ src/api/access.service.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import constants from '@/api/constants'
+import PentilaUtils from 'pentila-utils'
+
+export default {
+  getSchoolAccesses,
+  saveSchoolAccesses,
+  getUserAccesses,
+  getRoleAccesses
+}
+
+const ACCESS_PATH = '/access.'
+const ACCESS_CTX = 'access/'
+
+function getSchoolAccesses () {
+  return axios.get(constants.JSON_WS_URL + ACCESS_PATH + ACCESS_CTX + 'get-school-accesses', {
+    params: {
+    }
+  }).then(response => response.data)
+}
+
+function saveSchoolAccesses (schoolId, accesses) {
+  return axios.post(constants.JSON_WS_URL + ACCESS_PATH + ACCESS_CTX + 'save-school-accesses', PentilaUtils.URL.params({
+    schoolId: schoolId,
+    accesses: JSON.stringify(accesses)
+  })).then(response => response.data)
+}
+
+function getUserAccesses () {
+  return axios.get(constants.JSON_WS_URL + ACCESS_PATH + ACCESS_CTX + 'get-user-accesses', {
+    params: {
+    }
+  }).then(response => response.data)
+}
+
+function getRoleAccesses (roleId) {
+  return axios.get(constants.JSON_WS_URL + ACCESS_PATH + ACCESS_CTX + 'get-role-accesses', {
+    params: {
+      roleId
+    }
+  }).then(response => response.data)
+}
