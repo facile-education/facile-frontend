8d9df11622be951c95bd2069e90982736991e0e2
vdona <vdona@pentila.com>
2023-05-05 10:41:45 +0200

    [Dashboard - Activities] Add separator between news or not

--- src/components/Dashboard/ActivityWidget/ActivityWidget.vue
+++ src/components/Dashboard/ActivityWidget/ActivityWidget.vue
@@ -12,19 +12,38 @@
       class="placeholder"
     />
     <div
-      v-else-if="activities.length === 0 "
+      v-else-if="readActivities.length === 0 && unreadActivities.length === 0"
       v-t="'emptyPlaceholder'"
       class="placeholder"
     />
     <div v-else>
-      <ul>
+      <ul v-if="unreadActivities.length !== 0">
         <li
-          v-for="activity in activities"
+          v-for="activity in unreadActivities"
           :key="activity.activityId"
         >
           <ActivityItem
             :activity="activity"
-            :last-dashboard-access-date="lastDashboardAccessDate"
+            :is-unread="true"
+            @refresh="refresh"
+          />
+        </li>
+      </ul>
+
+      <div
+        v-if="unreadActivities.length !== 0 && readActivities.length !== 0"
+        class="separator theme-border-color"
+      >
+        {{ separatorLabel }}
+      </div>
+
+      <ul v-if="readActivities.length !== 0">
+        <li
+          v-for="activity in readActivities"
+          :key="activity.activityId"
+        >
+          <ActivityItem
+            :activity="activity"
             @refresh="refresh"
           />
         </li>
@@ -54,9 +73,15 @@ export default {
     return {
       isLoading: false,
       error: false,
-      activities: [],
       nbNewActivities: 0,
-      lastDashboardAccessDate: undefined
+      lastDashboardAccessDate: undefined,
+      readActivities: [],
+      unreadActivities: []
+    }
+  },
+  computed: {
+    separatorLabel () {
+      return this.$t('newsSince') + dayjs(this.lastDashboardAccessDate, 'YYYY-MM-DD HH:mm').format('DD MMMM YYYY')
     }
   },
   created () {
@@ -72,9 +97,18 @@ export default {
         this.isLoading = false
         if (data.success) {
           this.error = false
-          this.activities = data.activities
           this.nbNewActivities = data.nbNewActivities
           this.lastDashboardAccessDate = data.lastDashboardAccessDate
+          // this.lastDashboardAccessDate = '2022-05-05 10:19:48'
+          this.readActivities = []
+          this.unreadActivities = []
+          data.activities.forEach((activity) => {
+            if (this.isUnread(activity)) {
+              this.unreadActivities.push(activity)
+            } else {
+              this.readActivities.push(activity)
+            }
+          })
         } else {
           this.error = true
         }
@@ -82,6 +116,9 @@ export default {
         this.error = true
         console.error(err)
       })
+    },
+    isUnread (activity) {
+      return dayjs(activity.modificationDate, 'YYYY-MM-DD HH:mm').isAfter(dayjs(this.lastDashboardAccessDate, 'YYYY-MM-DD HH:mm'))
     }
   }
 }
@@ -111,6 +148,14 @@ ul {
   }
 }
 
+.separator {
+  font-size: 0.8rem;
+  color: $color-new-light-text;
+  border-bottom: 2px solid;
+  text-align: right;
+  margin: 10px 4px 8px 0;
+}
+
 .placeholder {
   height: 106px;
 }
@@ -124,6 +169,7 @@ ul {
 {
   "errorPlaceholder": "Oups, une erreur est survenue...",
   "emptyPlaceholder": "Aucune activité à afficher",
+  "newsSince": "Nouveautés depuis le ",
   "showMore": "Voir toutes les activités"
 }
 </i18n>
