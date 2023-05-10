875c8c6f8386e3388424a83a7556b2577271ac3f
vdona <vdona@pentila.com>
2023-05-10 11:35:12 +0200

    [Dashboard - Activities] Begin all activities

--- src/router/views/AllActivities.vue
+++ src/router/views/AllActivities.vue
@@ -1,10 +1,13 @@
 <template>
-  TODO: AllActivities
+  <ActivityWidget :display-all="true" />
 </template>
 
 <script>
+import ActivityWidget from '@components/Dashboard/ActivityWidget/ActivityWidget.vue'
+
 export default {
-  name: 'AllActivities'
+  name: 'AllActivities',
+  components: { ActivityWidget }
 }
 </script>
 
