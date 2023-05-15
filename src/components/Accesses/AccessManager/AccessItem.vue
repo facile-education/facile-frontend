543436724cd88def2d0cb60e27370090276ad81b
vdona <vdona@pentila.com>
2023-05-15 15:25:01 +0200

    [Access] Category modification and deletion

--- src/components/Accesses/AccessManager/AccessItem.vue
+++ src/components/Accesses/AccessManager/AccessItem.vue
@@ -4,15 +4,45 @@
       {{ access.title }}
     </h3>
   </div>
+
+  <teleport
+    v-if="isUpdateModalDisplayed"
+    to="body"
+  >
+    <SaveAccessModal
+      :category-list="categoryList"
+      :init-access="access"
+      @updateAccess="updateAccess"
+      @close="isUpdateModalDisplayed=false"
+    />
+  </teleport>
 </template>
 
 <script>
+import SaveAccessModal from '@components/Accesses/AccessManager/SaveAccessModal.vue'
+
 export default {
   name: 'AccessItem',
+  components: { SaveAccessModal },
   props: {
     access: {
       type: Object,
       required: true
+    }
+  },
+  data () {
+    return {
+      isUpdateModalDisplayed: false
+    }
+  },
+  computed: {
+    categoryList () {
+      return this.$store.state.accessManager.categoryList
+    }
+  },
+  methods: {
+    updateAccess () {
+      console.log('todo')
     }
   }
 }
