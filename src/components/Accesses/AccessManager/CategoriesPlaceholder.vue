c6555de46710332fea934b6c7b99b3feb017ea1a
vdona <vdona@pentila.com>
2023-05-22 11:46:27 +0200

    [Access] Create category from placeholder

---
+++ src/components/Accesses/AccessManager/CategoriesPlaceholder.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="placeholder">
+    <span v-t="'noAccess'" />
+    <em
+      v-t="'createAccess'"
+      @click="$emit('createCategory')"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoriesPlaceholder',
+  emits: ['createCategory']
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@design";
+
+.placeholder {
+  height: 20vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  text-align: center;
+  border: 1px solid $color-border;
+  border-radius: 6px;
+}
+
+em {
+  // TODO: Uniform placeholder style with progression
+  font-size: 0.9rem;
+  margin-top: 5px;
+  cursor: pointer;
+  color: $color-link;
+  text-decoration: underline;
+  font-style: normal;
+  font-weight: 600;
+}
+</style>
+
+<i18n locale="fr">
+{
+  "noAccess": "Aucun accès",
+  "createAccess": "Ajoutez votre première catégorie d'accès !"
+}
+</i18n>
