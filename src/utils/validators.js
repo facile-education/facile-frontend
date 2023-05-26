3c04060b3b6afd85ccaaf2941495d5bdb46db660
vdona <vdona@pentila.com>
2023-05-26 10:11:18 +0200

    Handle defaults thumbnail at some components

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -2,7 +2,8 @@ export default {
   notBeginByDot,
   containsNoCotes,
   isUnderMaxSize,
-  isNotEmpty
+  isNotEmpty,
+  isValidURL
 }
 
 function notBeginByDot (str) {
@@ -19,4 +20,14 @@ function isUnderMaxSize (str, maxSize) {
 
 function isNotEmpty (list) {
   return list.length > 0
+}
+
+function isValidURL (url) { // TODO: Move to utils?
+  const pattern = new RegExp('^(https?:\\/\\/)?' + // Protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // Domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // IP address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // Port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // Request string
+    '(\\#[-a-z\\d_]*)?$', 'i') // Fragment
+  return !!pattern.test(url)
 }
