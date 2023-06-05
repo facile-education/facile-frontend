a304bd55ffc3cf580e39eb5c107e864609588ca7
vdona <vdona@pentila.com>
2023-06-05 15:59:25 +0200

    [Image-Picker] Externalize filePickerButton

--- src/components/FilePicker/FilePickerButton.vue
+++ src/components/FilePicker/FilePickerButton.vue
@@ -1,75 +1,55 @@
 <template>
   <div
-    :id="id"
-    class="file-picker-button"
+    class="file-picker-button theme-background-color"
+    tabindex="0"
+    @click="$refs.file.click()"
+    @keyup.enter="$refs.file.click()"
   >
-    <input
-      class="input"
-      data-test="file-picker-button"
-      type="file"
-      :accept="accept"
-      :multiple="allowMultiple"
-      @change="fileAdded"
-    >
-    <span :v-if="label">
-      {{ label }}
+    <span class="button">
+      <input
+        ref="file"
+        type="file"
+        accept="image/*"
+        :multiple="allowMultiple"
+        @change="$emit('change', $event)"
+      >
     </span>
   </div>
 </template>
 
 <script>
 
-import utils from '@/utils/utils'
 export default {
   name: 'FilePickerButton',
   props: {
-    id: {
-      type: String,
-      required: true
-    },
     accept: {
       type: String,
       default: '*/*'
-    },
-    label: {
-      type: String,
-      default: ''
     },
     allowMultiple: {
       type: Boolean,
       default: false
     }
   },
-  emits: ['fileAdded'],
-  methods: {
-    fileAdded (e) {
-      utils.returnAddedFiles(e, this.$store).then((files) => {
-        if (files.length !== 0) {
-          this.$emit('fileAdded', files)
-        } else {
-          utils.alertNoFile()
-        }
-      })
-    }
-  }
+  emits: ['change']
 }
 </script>
 
 <style scoped>
-
   .file-picker-button{
-    display: inline-block;
-    height: 30px;
-    width: 150px;
-    background-color: #9effa1;
-    overflow: hidden;
+    height: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 2px 3px rgba(0,0,0,.19);
+    font-family: inherit;
+    border-radius: 5px;
+    padding: 6px 12px;
+    border: none;
     cursor: pointer;
   }
 
-  .input{
-    position: absolute;
-    opacity: 0;
-    height: 30px;
+  input {
+    display: none;
   }
-
 </style>
