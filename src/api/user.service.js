fe72ca96b69ebf0b5524d289e203b6c3495644a1
Marc Salvat <marc.salvat@weprode.com>
2023-06-06 16:49:56 +0200

    Improve password update with detailed error.

--- src/api/user.service.js
+++ src/api/user.service.js
@@ -11,7 +11,7 @@ export default {
   updateThemeColor,
   updateReportFrequency,
   updateWebdavState,
-  updateWebdavPassword,
+  updatePassword,
   acceptTermsOfUse
 }
 
@@ -112,11 +112,12 @@ function removeUserPicture () {
   ).then(response => response.data)
 }
 
-function updateWebdavPassword (password, confirmPassword) {
-  return axios.post(constants.JSON_WS_URL + PREF_PATH + '/update-webdav-password',
+function updatePassword (password, confirmPassword, isWebdav = false) {
+  return axios.post(constants.JSON_WS_URL + PREF_PATH + '/update-password',
     PentilaUtils.URL.params({
-      password: password,
-      confirmPassword: confirmPassword
+      password,
+      confirmPassword,
+      isWebdav
     })
   ).then(response => response.data)
 }
