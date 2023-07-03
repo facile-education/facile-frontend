18b9bea2eaaeb27127e1a70439140bad5a4f95fe
vdona <vdona@pentila.com>
2023-07-03 11:44:06 +0200

    [Global] Add SessionEndAdvertising modal

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -7,19 +7,7 @@
       @click="onShowMobileMenu"
     />
     <BannerSearch v-if="!mq.phone" />
-    <div
-      v-if="isSessionWarningDisplayed"
-      class="extend-session"
-    >
-      <span v-t="'session-is-expiring'" />
-      <span>{{ getRemainingTime }}</span>
-      <button
-        v-t="'extend-session'"
-        class="btn"
-        :title="$t('extend-session')"
-        @click="extendSession"
-      />
-    </div>
+
     <nav class="right-section">
       <BannerServices />
       <BannerUserProfile />
@@ -30,10 +18,7 @@
 <script>
 import BannerServices from '@components/Banner/BannerServices'
 import BannerUserProfile from '@components/Banner/BannerUserProfile'
-import dayjs from 'dayjs'
 import { defineAsyncComponent } from 'vue'
-
-import constants from '@/api/constants'
 
 const BannerSearch = defineAsyncComponent(() => import('@/components/Banner/BannerSearch'))
 const NeroIcon = defineAsyncComponent(() => import('@/components/Nero/NeroIcon'))
@@ -66,30 +51,9 @@ export default {
       return '' + nbMinutes + ':' + displayedSeconds
     }
   },
-  mounted () {
-    this.interval = setInterval(() => {
-      this.inactionTime = dayjs() - this.$store.state.user.lastActionDate
-      if (this.inactionTime > this.$store.state.menu.sessionTimeout) {
-        // Logout
-        window.location = constants.LOGOUT_URL
-      }
-    }, 1000)
-  },
-  unmounted () {
-    clearInterval(this.interval)
-  },
   methods: {
     onShowMobileMenu () {
       this.$store.dispatch('menu/toggleMobileMenu')
-    },
-    extendSession () {
-      fetch('/html/portal/extend_session.jsp').then(response => response.text()).then(response => {
-        const extendSessionResult = response.trim()
-
-        if (extendSessionResult === '0') {
-          this.$store.commit('user/setLastActionDate', dayjs())
-        }
-      })
     }
   }
 }
