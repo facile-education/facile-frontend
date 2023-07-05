ac60704b184d409b0bc740b0690667a10e8e657e
vdona <vdona@pentila.com>
2023-07-05 12:08:36 +0200

    [Global] Change notAllowed page style

--- src/router/views/NotAllowed.vue
+++ src/router/views/NotAllowed.vue
@@ -1,7 +1,14 @@
 <template>
-  <h2 class="msg">
-    {{ $t('notAllowed') }}
-  </h2>
+  <div class="authentication-required">
+    <div class="error">
+      <img
+        src="@/assets/icons/alert.svg"
+        alt=""
+      >
+      <h1 v-t="'notAllowed'" />
+      <div v-t="'useOthersServicesPlease'" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,13 +18,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.msg {
+@import "@design";
+
+.authentication-required {
+  padding-top: 30%;
+}
+
+.error {
+  position: relative;
   text-align: center;
+}
+
+h1 {
+  @extend %font-heading-s;
+}
+
+div {
+  @extend %font-regular-l;
+}
+
+img {
+  height: 24px;
 }
 </style>
 
 <i18n locale="fr">
 {
-  "notAllowed": "Vous ne possédez pas les droits requis pour accéder à ce service."
+  "notAllowed": "Vous ne disposez pas des autorisations pour accéder à ce service",
+  "useOthersServicesPlease": "Utilisez les services disponibles dans votre menu"
 }
 </i18n>
