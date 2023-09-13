4c83779509b0fcc6a17ceefa1b1344021d2d72b0
vdona <vdona@pentila.com>
2023-09-13 10:29:14 +0200

    [Version notes] Add spinner and error messages on SaveVersionNoteModal

--- src/components/VersionNotes/SaveVersionNoteModal.vue
+++ src/components/VersionNotes/SaveVersionNoteModal.vue
@@ -3,6 +3,7 @@
     :modal="true"
     :draggable="true"
     :full-screen="mq.phone"
+    class="save-version-note-modal"
     @close="$emit('close')"
   >
     <template #header>
@@ -10,6 +11,8 @@
     </template>
 
     <template #body>
+      <PentilaSpinner v-if="isLoading" />
+
       <div class="title">
         <PentilaInput
           v-model="title"
@@ -71,7 +74,8 @@ export default {
     return {
       title: undefined,
       htmlContent: undefined,
-      editor: InlineEditor
+      editor: InlineEditor,
+      isLoading: false
     }
   },
   computed: {
@@ -104,26 +108,50 @@ export default {
       }
     },
     createVersionNote () {
+      this.isLoading = true
       createVersionNote(this.title, this.htmlContent).then((data) => {
+        this.isLoading = false
         if (data.success) {
           this.$store.dispatch('about/getVersionNotesList')
           this.$emit('close')
+        } else {
+          this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
+          console.error('Cannot create versionNote')
         }
-        // TODO: handle errors
+      }, (err) => {
+        this.isLoading = false
+        this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
+        console.error(err)
       })
     },
     updateVersionNote () {
+      this.isLoading = true
       updateVersionNote(this.selectedNote.versionNoteId, this.title, this.htmlContent).then((data) => {
+        this.isLoading = false
         if (data.success) {
           this.$store.dispatch('about/getVersionNoteContent', this.selectedNote)
           this.$emit('close')
+        } else {
+          console.error('Cannot update versionNote')
+          this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
         }
-        // TODO: handle errors
+      }, (err) => {
+        this.isLoading = false
+        this.$store.dispatch('popups/pushPopup', { message: this.$t('error'), type: 'error' })
+        console.error(err)
       })
     }
   }
 }
 </script>
+
+<style lang="scss">
+.save-version-note-modal {
+  .window-body {
+    position: relative;
+  }
+}
+</style>
 
 <style lang="scss" scoped>
 @import "@design";
@@ -148,6 +176,7 @@ export default {
   "contentLabel": "Contenu:",
   "create": "Créer",
   "createHeader": "Création d'une note de version",
+  "error": "Échec de la requète",
   "required": "Champs requis",
   "titlePlaceholder": "Titre",
   "update": "Modifier",
