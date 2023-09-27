c28fa0c4bd336eca12f6dad19234d360a82c0c64
vdona <vdona@pentila.com>
2023-09-27 15:56:25 +0200

    [Tests] Add eslint plugin for cypress

--- tests/e2e/.eslintrc.js
+++ tests/e2e/.eslintrc.js
@@ -6,7 +6,16 @@ module.exports = {
     mocha: true,
     'cypress/globals': true
   },
+  extends: [
+    'plugin:cypress/recommended'
+  ],
   rules: {
     strict: 'off'
+    // 'cypress/no-assigning-return-values': 'error',
+    // 'cypress/no-unnecessary-waiting': 'error'
+    // 'cypress/assertion-before-screenshot': 'warn',
+    // 'cypress/no-force': 'warn',
+    // 'cypress/no-async-tests': 'error',
+    // 'cypress/no-pause': 'error'
   }
 }
--- tests/e2e/integration/login.spec.js
+++ tests/e2e/integration/login.spec.js
@@ -26,14 +26,16 @@ describe('Login page', () => {
     cy.get('.errorMessage').should('be.visible')
   })
 
-  it.skip('Locks account after 5 unsuccessful attempts', () => {  // TODO: Need to clean DB state after
+  it.skip('Locks account after 5 unsuccessful attempts', () => { // TODO: Need to clean DB state after
     cy.visit('/login')
     cy.contains('Parents / Autres profils').click()
 
     // Add a loop to simulate 5 unsuccessful login attempts
     for (let i = 0; i < 5; i++) {
-      cy.get('input[placeholder="Identifiant"]').clear().type(user.login)
-      cy.get('input[type="password"]').clear().type(user.password + '_')
+      cy.get('input[placeholder="Identifiant"]').clear()
+      cy.get('input[placeholder="Identifiant"]').type(user.login)
+      cy.get('input[type="password"]').clear()
+      cy.get('input[type="password"]').type(user.password + '_')
       cy.get('button[type="submit"]').click()
 
       // Check for error message after each attempt
