e5a7c0e88b9aab911dfc7a85e05940b6b59ca74c
vdona <vdona@pentila.com>
2023-10-04 15:29:46 +0200

    [Tests] Review login to use api methods

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -32,15 +32,38 @@ Cypress.Commands.add('login', (user, visitUrl = undefined) => {
   cy.log('===== LOG IN (' + user.login + ') =====')
 
   cy.session(user, () => {
+    cy.setCookie('COOKIE_SUPPORT', 'true')
     cy.setCookie('cookiesAgreement', 'true')
-    // UI method, TODO: api method
-    cy.visit('/login')
-    cy.contains('Parents / Autres profils').click()
 
-    cy.get('input[placeholder="Identifiant"]').type(user.login)
-    cy.get('input[type="password"]').type(user.password)
-    cy.get('button[type="submit"]').click()
-    // cy.url().should('contain', '/tableau-de-bord')
+    cy.request({ // get pauth
+      method: 'GET',
+      url: Cypress.config().baseUrl + '/lfr/p_auth_token.jsp',
+      headers: {
+        Referer: Cypress.config().baseUrl + '/lfr/'
+      }
+    }).then((response) => {
+      // expect(response.status).to.eq(200) // to test here or not?
+      const pAuth = response.body.replace(/(\r\n|\n|\r|\s)/gm, '')
+      const loginUrl = Cypress.config().baseUrl + '/lfr/c/common/login'
+
+      const params = {
+        login: user.login,
+        password: user.password,
+        rememberMe: false,
+        p_auth: pAuth
+      }
+
+      cy.request({
+        method: 'POST',
+        url: loginUrl,
+        form: true,
+        body: params
+      }).then((response) => {
+        console.log(response)
+        expect(response.status).to.be.oneOf([200, 304])
+        expect(response.body.success).to.be.eq(true)
+      })
+    })
   },
   {
     // TODO: Review validation method (check status of get-user-info by example / check if cookie sessionId is not null...)
@@ -60,64 +83,6 @@ Cypress.Commands.add('login', (user, visitUrl = undefined) => {
   if (visitUrl && window.location.href !== Cypress.config().baseUrl + visitUrl) {
     cy.visit(visitUrl)
   }
-
-  // Request method (works only if there is no redirection on login request that ends in 404)
-  // // Get p_auth
-  // cy.request({
-  //   method: 'GET',
-  //   url: Cypress.config().baseUrl + '/p_auth_token.jsp',
-  //   headers: {
-  //     Referer: 'https://dev-ent-gve.com/nero/'
-  //   }
-  // }).then((response) => {
-  //   // expect(response.status).to.eq(200) // to test here or not?
-  //   const pAuth = response.body.replace(/(\r\n|\n|\r|\s)/gm, '')
-  //
-  //   const params = {
-  //     _com_liferay_login_web_portlet_LoginPortlet_formDate: Date.now(),
-  //     _com_liferay_login_web_portlet_LoginPortlet_saveLastPath: false,
-  //     _com_liferay_login_web_portlet_LoginPortlet_redirect: '',
-  //     _com_liferay_login_web_portlet_LoginPortlet_doActionAfterLogin: false,
-  //     _com_liferay_login_web_portlet_LoginPortlet_login: user.login,
-  //     _com_liferay_login_web_portlet_LoginPortlet_password: user.password,
-  //     _com_liferay_login_web_portlet_LoginPortlet_checkboxNames: 'rememberMe',
-  //     p_auth: pAuth
-  //   }
-  //
-  //   const loginUrl = Cypress.config().baseUrl + '/home?' +
-  //     'p_p_id=com_liferay_login_web_portlet_LoginPortlet' +
-  //     '&p_p_lifecycle=1&' +
-  //     'p_p_state=exclusive&' +
-  //     'p_p_mode=view&' +
-  //     '_com_liferay_login_web_portlet_LoginPortlet_javax.portlet.action=%2Flogin%2Flogin&' +
-  //     '_com_liferay_login_web_portlet_LoginPortlet_mvcRenderCommandName=%2Flogin%2Flogin'
-  //
-  //   cy.request({
-  //     method: 'POST',
-  //     url: loginUrl,
-  //     form: true,
-  //     body: params,
-  //     headers: {
-  //       Origin: 'https://dev-ent-gve.com'
-  //     }
-  //   }).then((authResponse) => {
-  //     expect(authResponse.status).to.eq(200) // to test here or not?
-  //
-  //     console.log(authResponse.body)
-  //
-  //     // "GUEST_LANGUAGE_ID=fr_FR; COOKIE_SUPPORT=true; JSESSIONID=DACF51174FD494C169047630C1A1A34D"
-  //     // "COOKIE_SUPPORT=true; LFR_SESSION_STATE_11105=1684919766820; JSESSIONID=5C92E79BDDF250CAC7038A0C6BBEB906; GUEST_LANGUAGE_ID=fr_FR; LFR_SESSION_STATE_10205=1684919857948"
-  //
-  //     cy.wait(1000) // to be sure
-  //
-  //     cy.visit(visitUrl)
-  //     if (visitUrl === '/') {
-  //       cy.url().should('contain', Cypress.config().baseUrl + visitUrl) // because of the '/tableau-de-board' redirection
-  //     } else {
-  //       cy.url().should('eq', Cypress.config().baseUrl + visitUrl)
-  //     }
-  //   })
-  // })
 })
 
 Cypress.Commands.add('loadTables', (dumpName) => {
@@ -141,7 +106,6 @@ Cypress.Commands.add('loadTables', (dumpName) => {
   }).then((response) => {
     // expect(response.status).to.eq(200) // to test here or not?
     const pAuth = response.body.replace(/(\r\n|\n|\r|\s)/gm, '')
-
     const url = Cypress.config().baseUrl + '/lfr/group/control_panel/manage?' +
     'p_p_id=com_liferay_server_admin_web_portlet_ServerAdminPortlet&' +
     'p_p_lifecycle=1&' +
@@ -164,7 +128,6 @@ Cypress.Commands.add('loadTables', (dumpName) => {
       body: params
     }).then((response) => {
       expect(response.status).to.be.oneOf([200, 304])
-      console.log(response)
     })
   })
 })
