42f00d54e6c14a6a707acaa4e751e921bb16dc43
vdona <vdona@pentila.com>
2023-10-25 16:53:40 +0200

    [Messaging - tests] Remove database name in dumps files

--- tests/e2e/scripts/tables/messaging/messaging_tables.sql
+++ tests/e2e/scripts/tables/messaging/messaging_tables.sql
@@ -52,7 +52,7 @@ CREATE TABLE `Messaging_Message` (
 
 LOCK TABLES `Messaging_Message` WRITE;
 /*!40000 ALTER TABLE `Messaging_Message` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
+INSERT INTO Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
 	 (485621,534218,485622,0,58811,'2023-10-23 14:28:32.349','Brouillon existant','Ceci est un brouillon déjà existant',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (490701,19662701,490702,0,52216,'2023-10-24 16:06:16.840','Message dans un dossier','Contenu du message dans un dossier',0,NULL,0,0,'Stanislaus ROPKES',0,20097),
 	 (490706,19662702,490707,0,52216,'2023-10-24 16:07:20.354','Message dans un sous dossier','Contenu du message dans un sous dossier',0,NULL,0,0,'Stanislaus ROPKES',0,20097),
@@ -63,7 +63,7 @@ INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessa
 	 (534219,534216,534214,0,58811,'2023-10-05 08:54:09.390','Re: deuxième message','Je vous réponds!',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (534220,534206,534214,534219,58811,'2023-10-05 08:54:09.499','Re: deuxième message','Je vous réponds!',0,'2023-10-05 08:54:17',1,0,'Orlan HUE',0,20097),
 	 (534221,534207,534214,0,45479,'2023-10-05 08:54:27.568','Re: deuxième message','Merci',0,NULL,0,0,'Rosana NOLLI',0,20097);
-INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
+INSERT INTO Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
 	 (534222,534205,534214,534221,45479,'2023-10-05 08:54:27.634','Re: deuxième message','Merci',1,NULL,0,0,'Rosana NOLLI',0,20097),
 	 (534223,534207,534224,0,45479,'2023-10-05 09:01:19.210','Troisième message','J''ai encore des choses à dire!',0,NULL,0,0,'Rosana NOLLI',0,20097),
 	 (534225,534205,534224,534223,45479,'2023-10-05 09:01:19.272','Troisième message','J''ai encore des choses à dire!',1,NULL,0,0,'Rosana NOLLI',0,20097),
@@ -118,7 +118,7 @@ CREATE TABLE `Messaging_MessageContent` (
 
 LOCK TABLES `Messaging_MessageContent` WRITE;
 /*!40000 ALTER TABLE `Messaging_MessageContent` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALUES
+INSERT INTO Messaging_MessageContent (messageId,messageContent) VALUES
 	 (485621,'<html>
  <head></head>
 <body>
@@ -179,7 +179,7 @@ INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALU
  <p>Merci</p>
 </body></body>
 </html>');
-INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALUES
+INSERT INTO Messaging_MessageContent (messageId,messageContent) VALUES
 	 (534222,'<html>
  <head></head>
 <body>
@@ -245,7 +245,7 @@ CREATE TABLE `Messaging_MessageFolder` (
 
 LOCK TABLES `Messaging_MessageFolder` WRITE;
 /*!40000 ALTER TABLE `Messaging_MessageFolder` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
+INSERT INTO Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
 	 (534201,47362,'Boîte de réception',1,0),
 	 (534202,47362,'Envoyés',3,0),
 	 (534203,47362,'Corbeille',4,0),
@@ -256,7 +256,7 @@ INSERT INTO lportal_ent.Messaging_MessageFolder (folderId,userId,folderName,type
 	 (534208,45479,'Corbeille',4,0),
 	 (534209,45479,'Brouillons',2,0),
 	 (534216,58811,'Envoyés',3,0);
-INSERT INTO lportal_ent.Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
+INSERT INTO Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
 	 (534217,58811,'Corbeille',4,0),
 	 (534218,58811,'Brouillons',2,0),
 	 (538201,58811,'sous-dossier',5,0),
@@ -267,7 +267,7 @@ INSERT INTO lportal_ent.Messaging_MessageFolder (folderId,userId,folderName,type
 	 (19654704,52216,'Brouillons',2,0),
 	 (19662701,52216,'dossier personnel',5,0),
 	 (19662702,52216,'sous-dossier',5,19662701);
-INSERT INTO lportal_ent.Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
+INSERT INTO Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
 	 (19662703,52216,'Mon autre dossier',5,0);
 /*!40000 ALTER TABLE `Messaging_MessageFolder` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -293,7 +293,7 @@ CREATE TABLE `Messaging_MessageRecipients` (
 
 LOCK TABLES `Messaging_MessageRecipients` WRITE;
 /*!40000 ALTER TABLE `Messaging_MessageRecipients` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUES
+INSERT INTO Messaging_MessageRecipients (messageId,recipients) VALUES
 	 (485621,'47461,'),
 	 (490701,'52216,'),
 	 (490706,'52216,'),
@@ -304,7 +304,7 @@ INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUE
 	 (534219,'45479,'),
 	 (534220,'45479,'),
 	 (534221,'58811,');
-INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUES
+INSERT INTO Messaging_MessageRecipients (messageId,recipients) VALUES
 	 (534222,'58811,'),
 	 (534223,'58811,'),
 	 (534225,'58811,'),
--- tests/e2e/scripts/tables/messaging/messaging_tables_full.sql
+++ tests/e2e/scripts/tables/messaging/messaging_tables_full.sql
@@ -52,7 +52,7 @@ CREATE TABLE `Messaging_Message` (
 
 LOCK TABLES `Messaging_Message` WRITE;
 /*!40000 ALTER TABLE `Messaging_Message` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
+INSERT INTO Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
 	 (492505,492502,492506,0,58811,'2023-10-25 10:39:10.458','Message 0','Contenu du message 0',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492508,492507,492506,492505,58811,'2023-10-25 10:39:10.517','Message 0','Contenu du message 0',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492509,492502,492510,0,58811,'2023-10-25 10:39:11.945','Message 1','Contenu du message 1',0,NULL,0,0,'Orlan HUE',0,20097),
@@ -63,7 +63,7 @@ INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessa
 	 (492517,492507,492516,492515,58811,'2023-10-25 10:39:14.810','Message 3','Contenu du message 3',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492518,492502,492519,0,58811,'2023-10-25 10:39:16.239','Message 4','Contenu du message 4',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492520,492507,492519,492518,58811,'2023-10-25 10:39:16.286','Message 4','Contenu du message 4',1,NULL,0,0,'Orlan HUE',0,20097);
-INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
+INSERT INTO Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
 	 (492521,492502,492522,0,58811,'2023-10-25 10:39:17.848','Message 5','Contenu du message 5',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492523,492507,492522,492521,58811,'2023-10-25 10:39:17.892','Message 5','Contenu du message 5',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492524,492502,492525,0,58811,'2023-10-25 10:39:19.405','Message 6','Contenu du message 6',0,NULL,0,0,'Orlan HUE',0,20097),
@@ -74,7 +74,7 @@ INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessa
 	 (492532,492507,492531,492530,58811,'2023-10-25 10:39:22.653','Message 8','Contenu du message 8',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492533,492502,492534,0,58811,'2023-10-25 10:39:24.385','Message 9','Contenu du message 9',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492535,492507,492534,492533,58811,'2023-10-25 10:39:24.417','Message 9','Contenu du message 9',1,NULL,0,0,'Orlan HUE',0,20097);
-INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
+INSERT INTO Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
 	 (492536,492502,492537,0,58811,'2023-10-25 10:39:26.196','Message 10','Contenu du message 10',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492538,492507,492537,492536,58811,'2023-10-25 10:39:26.232','Message 10','Contenu du message 10',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492539,492502,492540,0,58811,'2023-10-25 10:39:28.025','Message 11','Contenu du message 11',0,NULL,0,0,'Orlan HUE',0,20097),
@@ -85,7 +85,7 @@ INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessa
 	 (492547,492507,492546,492545,58811,'2023-10-25 10:39:32.006','Message 13','Contenu du message 13',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492548,492502,492549,0,58811,'2023-10-25 10:39:33.976','Message 14','Contenu du message 14',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492550,492507,492549,492548,58811,'2023-10-25 10:39:34.008','Message 14','Contenu du message 14',1,NULL,0,0,'Orlan HUE',0,20097);
-INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
+INSERT INTO Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
 	 (492551,492502,492552,0,58811,'2023-10-25 10:39:35.846','Message 15','Contenu du message 15',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492553,492507,492552,492551,58811,'2023-10-25 10:39:35.876','Message 15','Contenu du message 15',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492554,492502,492555,0,58811,'2023-10-25 10:39:37.911','Message 16','Contenu du message 16',0,NULL,0,0,'Orlan HUE',0,20097),
@@ -96,7 +96,7 @@ INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessa
 	 (492562,492507,492561,492560,58811,'2023-10-25 10:39:42.177','Message 18','Contenu du message 18',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492563,492502,492564,0,58811,'2023-10-25 10:39:44.351','Message 19','Contenu du message 19',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492565,492507,492564,492563,58811,'2023-10-25 10:39:44.385','Message 19','Contenu du message 19',1,NULL,0,0,'Orlan HUE',0,20097);
-INSERT INTO lportal_ent.Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
+INSERT INTO Messaging_Message (messageId,folderId,threadId,sendMessageId,senderId,sendDate,messageSubject,messageContent,isNew,readDate,isAnswered,isForwarded,senderName,type_,companyId) VALUES
 	 (492566,492502,492567,0,58811,'2023-10-25 10:39:46.671','Message 20','Contenu du message 20',0,NULL,0,0,'Orlan HUE',0,20097),
 	 (492568,492507,492567,492566,58811,'2023-10-25 10:39:46.708','Message 20','Contenu du message 20',1,NULL,0,0,'Orlan HUE',0,20097),
 	 (492569,492502,492570,0,58811,'2023-10-25 10:39:48.789','Message 21','Contenu du message 21',0,NULL,0,0,'Orlan HUE',0,20097),
@@ -155,309 +155,309 @@ CREATE TABLE `Messaging_MessageContent` (
 
 LOCK TABLES `Messaging_MessageContent` WRITE;
 /*!40000 ALTER TABLE `Messaging_MessageContent` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALUES
+INSERT INTO Messaging_MessageContent (messageId,messageContent) VALUES
 	 (492505,'<html>
  <head></head>
 <body>
-  Contenu du message 0 
+  Contenu du message 0
 </body></body>
 </html>'),
 	 (492508,'<html>
  <head></head>
 <body>
-  Contenu du message 0 
+  Contenu du message 0
 </body></body>
 </html>'),
 	 (492509,'<html>
  <head></head>
 <body>
-  Contenu du message 1 
+  Contenu du message 1
 </body></body>
 </html>'),
 	 (492511,'<html>
  <head></head>
 <body>
-  Contenu du message 1 
+  Contenu du message 1
 </body></body>
 </html>'),
 	 (492512,'<html>
  <head></head>
 <body>
-  Contenu du message 2 
+  Contenu du message 2
 </body></body>
 </html>'),
 	 (492514,'<html>
  <head></head>
 <body>
-  Contenu du message 2 
+  Contenu du message 2
 </body></body>
 </html>'),
 	 (492515,'<html>
  <head></head>
 <body>
-  Contenu du message 3 
+  Contenu du message 3
 </body></body>
 </html>'),
 	 (492517,'<html>
  <head></head>
 <body>
-  Contenu du message 3 
+  Contenu du message 3
 </body></body>
 </html>'),
 	 (492518,'<html>
  <head></head>
 <body>
-  Contenu du message 4 
+  Contenu du message 4
 </body></body>
 </html>'),
 	 (492520,'<html>
  <head></head>
 <body>
-  Contenu du message 4 
+  Contenu du message 4
 </body></body>
 </html>');
-INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALUES
+INSERT INTO Messaging_MessageContent (messageId,messageContent) VALUES
 	 (492521,'<html>
  <head></head>
 <body>
-  Contenu du message 5 
+  Contenu du message 5
 </body></body>
 </html>'),
 	 (492523,'<html>
  <head></head>
 <body>
-  Contenu du message 5 
+  Contenu du message 5
 </body></body>
 </html>'),
 	 (492524,'<html>
  <head></head>
 <body>
-  Contenu du message 6 
+  Contenu du message 6
 </body></body>
 </html>'),
 	 (492526,'<html>
  <head></head>
 <body>
-  Contenu du message 6 
+  Contenu du message 6
 </body></body>
 </html>'),
 	 (492527,'<html>
  <head></head>
 <body>
-  Contenu du message 7 
+  Contenu du message 7
 </body></body>
 </html>'),
 	 (492529,'<html>
  <head></head>
 <body>
-  Contenu du message 7 
+  Contenu du message 7
 </body></body>
 </html>'),
 	 (492530,'<html>
  <head></head>
 <body>
-  Contenu du message 8 
+  Contenu du message 8
 </body></body>
 </html>'),
 	 (492532,'<html>
  <head></head>
 <body>
-  Contenu du message 8 
+  Contenu du message 8
 </body></body>
 </html>'),
 	 (492533,'<html>
  <head></head>
 <body>
-  Contenu du message 9 
+  Contenu du message 9
 </body></body>
 </html>'),
 	 (492535,'<html>
  <head></head>
 <body>
-  Contenu du message 9 
+  Contenu du message 9
 </body></body>
 </html>');
-INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALUES
+INSERT INTO Messaging_MessageContent (messageId,messageContent) VALUES
 	 (492536,'<html>
  <head></head>
 <body>
-  Contenu du message 10 
+  Contenu du message 10
 </body></body>
 </html>'),
 	 (492538,'<html>
  <head></head>
 <body>
-  Contenu du message 10 
+  Contenu du message 10
 </body></body>
 </html>'),
 	 (492539,'<html>
  <head></head>
 <body>
-  Contenu du message 11 
+  Contenu du message 11
 </body></body>
 </html>'),
 	 (492541,'<html>
  <head></head>
 <body>
-  Contenu du message 11 
+  Contenu du message 11
 </body></body>
 </html>'),
 	 (492542,'<html>
  <head></head>
 <body>
-  Contenu du message 12 
+  Contenu du message 12
 </body></body>
 </html>'),
 	 (492544,'<html>
  <head></head>
 <body>
-  Contenu du message 12 
+  Contenu du message 12
 </body></body>
 </html>'),
 	 (492545,'<html>
  <head></head>
 <body>
-  Contenu du message 13 
+  Contenu du message 13
 </body></body>
 </html>'),
 	 (492547,'<html>
  <head></head>
 <body>
-  Contenu du message 13 
+  Contenu du message 13
 </body></body>
 </html>'),
 	 (492548,'<html>
  <head></head>
 <body>
-  Contenu du message 14 
+  Contenu du message 14
 </body></body>
 </html>'),
 	 (492550,'<html>
  <head></head>
 <body>
-  Contenu du message 14 
+  Contenu du message 14
 </body></body>
 </html>');
-INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALUES
+INSERT INTO Messaging_MessageContent (messageId,messageContent) VALUES
 	 (492551,'<html>
  <head></head>
 <body>
-  Contenu du message 15 
+  Contenu du message 15
 </body></body>
 </html>'),
 	 (492553,'<html>
  <head></head>
 <body>
-  Contenu du message 15 
+  Contenu du message 15
 </body></body>
 </html>'),
 	 (492554,'<html>
  <head></head>
 <body>
-  Contenu du message 16 
+  Contenu du message 16
 </body></body>
 </html>'),
 	 (492556,'<html>
  <head></head>
 <body>
-  Contenu du message 16 
+  Contenu du message 16
 </body></body>
 </html>'),
 	 (492557,'<html>
  <head></head>
 <body>
-  Contenu du message 17 
+  Contenu du message 17
 </body></body>
 </html>'),
 	 (492559,'<html>
  <head></head>
 <body>
-  Contenu du message 17 
+  Contenu du message 17
 </body></body>
 </html>'),
 	 (492560,'<html>
  <head></head>
 <body>
-  Contenu du message 18 
+  Contenu du message 18
 </body></body>
 </html>'),
 	 (492562,'<html>
  <head></head>
 <body>
-  Contenu du message 18 
+  Contenu du message 18
 </body></body>
 </html>'),
 	 (492563,'<html>
  <head></head>
 <body>
-  Contenu du message 19 
+  Contenu du message 19
 </body></body>
 </html>'),
 	 (492565,'<html>
  <head></head>
 <body>
-  Contenu du message 19 
+  Contenu du message 19
 </body></body>
 </html>');
-INSERT INTO lportal_ent.Messaging_MessageContent (messageId,messageContent) VALUES
+INSERT INTO Messaging_MessageContent (messageId,messageContent) VALUES
 	 (492566,'<html>
  <head></head>
 <body>
-  Contenu du message 20 
+  Contenu du message 20
 </body></body>
 </html>'),
 	 (492568,'<html>
  <head></head>
 <body>
-  Contenu du message 20 
+  Contenu du message 20
 </body></body>
 </html>'),
 	 (492569,'<html>
  <head></head>
 <body>
-  Contenu du message 21 
+  Contenu du message 21
 </body></body>
 </html>'),
 	 (492571,'<html>
  <head></head>
 <body>
-  Contenu du message 21 
+  Contenu du message 21
 </body></body>
 </html>'),
 	 (492572,'<html>
  <head></head>
 <body>
-  Contenu du message 22 
+  Contenu du message 22
 </body></body>
 </html>'),
 	 (492574,'<html>
  <head></head>
 <body>
-  Contenu du message 22 
+  Contenu du message 22
 </body></body>
 </html>'),
 	 (492575,'<html>
  <head></head>
 <body>
-  Contenu du message 23 
+  Contenu du message 23
 </body></body>
 </html>'),
 	 (492577,'<html>
  <head></head>
 <body>
-  Contenu du message 23 
+  Contenu du message 23
 </body></body>
 </html>'),
 	 (492578,'<html>
  <head></head>
 <body>
-  Contenu du message 24 
+  Contenu du message 24
 </body></body>
 </html>'),
 	 (492580,'<html>
  <head></head>
 <body>
-  Contenu du message 24 
+  Contenu du message 24
 </body></body>
 </html>');
 /*!40000 ALTER TABLE `Messaging_MessageContent` ENABLE KEYS */;
@@ -489,7 +489,7 @@ CREATE TABLE `Messaging_MessageFolder` (
 
 LOCK TABLES `Messaging_MessageFolder` WRITE;
 /*!40000 ALTER TABLE `Messaging_MessageFolder` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
+INSERT INTO Messaging_MessageFolder (folderId,userId,folderName,type_,parentFolderId) VALUES
 	 (492501,58811,'Boîte de réception',1,0),
 	 (492502,58811,'Envoyés',3,0),
 	 (492503,58811,'Corbeille',4,0),
@@ -519,7 +519,7 @@ CREATE TABLE `Messaging_MessageRecipients` (
 
 LOCK TABLES `Messaging_MessageRecipients` WRITE;
 /*!40000 ALTER TABLE `Messaging_MessageRecipients` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUES
+INSERT INTO Messaging_MessageRecipients (messageId,recipients) VALUES
 	 (492505,'47461,'),
 	 (492508,'47461,'),
 	 (492509,'47461,'),
@@ -530,7 +530,7 @@ INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUE
 	 (492517,'47461,'),
 	 (492518,'47461,'),
 	 (492520,'47461,');
-INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUES
+INSERT INTO Messaging_MessageRecipients (messageId,recipients) VALUES
 	 (492521,'47461,'),
 	 (492523,'47461,'),
 	 (492524,'47461,'),
@@ -541,7 +541,7 @@ INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUE
 	 (492532,'47461,'),
 	 (492533,'47461,'),
 	 (492535,'47461,');
-INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUES
+INSERT INTO Messaging_MessageRecipients (messageId,recipients) VALUES
 	 (492536,'47461,'),
 	 (492538,'47461,'),
 	 (492539,'47461,'),
@@ -552,7 +552,7 @@ INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUE
 	 (492547,'47461,'),
 	 (492548,'47461,'),
 	 (492550,'47461,');
-INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUES
+INSERT INTO Messaging_MessageRecipients (messageId,recipients) VALUES
 	 (492551,'47461,'),
 	 (492553,'47461,'),
 	 (492554,'47461,'),
@@ -563,7 +563,7 @@ INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUE
 	 (492562,'47461,'),
 	 (492563,'47461,'),
 	 (492565,'47461,');
-INSERT INTO lportal_ent.Messaging_MessageRecipients (messageId,recipients) VALUES
+INSERT INTO Messaging_MessageRecipients (messageId,recipients) VALUES
 	 (492566,'47461,'),
 	 (492568,'47461,'),
 	 (492569,'47461,'),
@@ -602,7 +602,7 @@ CREATE TABLE `Messaging_MessagingConfig` (
 
 LOCK TABLES `Messaging_MessagingConfig` WRITE;
 /*!40000 ALTER TABLE `Messaging_MessagingConfig` DISABLE KEYS */;
-INSERT INTO lportal_ent.Messaging_MessagingConfig (userId,isForwardActive,forwardMail,isSignatureActive,signature,isAutoReplyActive,autoReplyContent) VALUES
+INSERT INTO Messaging_MessagingConfig (userId,isForwardActive,forwardMail,isSignatureActive,signature,isAutoReplyActive,autoReplyContent) VALUES
 	 (47461,1,'penelope.rbr@eduge.loc',0,NULL,0,NULL),
 	 (332442,1,'darko.jovanovic@test.ge.ch',1,'<p>My signature</p>',1,'<p>My auto reply text</p>'),
 	 (19279640,1,'fiheri@weprode.com',0,NULL,0,NULL),
