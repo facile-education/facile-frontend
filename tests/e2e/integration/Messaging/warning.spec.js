aadd8d5330f692c7587fbe7a0fab7ee64eec22cf
Evan <evanchauffour@gmail.com>
2023-10-26 15:34:50 +0200

    [tests - messaging] Add test warning on reply / replyAll / forward / newMessage

--- tests/e2e/integration/Messaging/warning.spec.js
+++ tests/e2e/integration/Messaging/warning.spec.js
@@ -1,6 +1,6 @@
 import { messagingURL } from '../../support/constants/urls'
-import { HEADMASTER } from '../../support/constants/users'
-import { getThread, waitMessagingToBeLoaded } from '../../support/utils/messagingUtils'
+import { HEADMASTER, TEACHER } from '../../support/constants/users'
+import { getThread, setRecipient, waitMessagingToBeLoaded } from '../../support/utils/messagingUtils'
 
 describe('Warning', () => {
   beforeEach(() => {
@@ -16,20 +16,61 @@ describe('Warning', () => {
     cy.login(HEADMASTER, messagingURL)
     waitMessagingToBeLoaded()
     const existingDraftThreads = this.messagingData.existingDraftThreads[0]
-    const ModifiedDraftContent = this.messagingData.ModifiedDraftContent[0]
+    const ModifiedDraft = this.messagingData.ModifiedDraftContent[0]
 
     // Go to draft menu
     cy.get('[data-test="option_toggleMessagingMenu"]').click()
     cy.get('[data-test="messaging-menu"]').contains('button', 'Brouillons').click()
 
-    // Click on existing draft
+    // Click on existing draft to modify content
     getThread(existingDraftThreads).click()
     cy.get('[data-test="option_editDraft"]').click()
 
     // content modified
     cy.get('[data-test="createMessageModal"]').within(() => {
       cy.get('.ck-editor')
-      cy.type_ckeditor(ModifiedDraftContent[0].content)
+      cy.type_ckeditor(ModifiedDraft[0].content)
+
+      // Close modal
+      cy.get('[data-test="closeModal"]').click()
+    })
+    // Check if warning modal is visible
+    cy.get('[data-test="warning-modal"]').should('be.exist')
+    // Click on confirm button
+    cy.get('[data-test="confirmButton"]').click()
+    // Check if warning modal is not visible
+    cy.get('[data-test="warning-modal"]').should('not.exist')
+    // Check if modal create message is not visible
+    cy.get('[data-test="createMessageModal"]').should('not.exist')
+
+    // Click on existing draft to modify subject
+    getThread(existingDraftThreads).click()
+    cy.get('[data-test="option_editDraft"]').click()
+
+    // subject modified
+    cy.get('[data-test="createMessageModal"]').within(() => {
+      cy.get('.group > [data-test="subject-input"]').clear()
+      cy.get('.group > [data-test="subject-input"]').type(ModifiedDraft[0].subject)
+
+      // Close modal
+      cy.get('[data-test="closeModal"]').click()
+    })
+    // Check if warning modal is visible
+    cy.get('[data-test="warning-modal"]').should('be.exist')
+    // Click on confirm button
+    cy.get('[data-test="confirmButton"]').click()
+    // Check if warning modal is not visible
+    cy.get('[data-test="warning-modal"]').should('not.exist')
+    // Check if modal create message is not visible
+    cy.get('[data-test="createMessageModal"]').should('not.exist')
+
+    // Click on existing draft to modify recipient
+    getThread(existingDraftThreads).click()
+    cy.get('[data-test="option_editDraft"]').click()
+
+    // recipient modified
+    cy.get('[data-test="createMessageModal"]').within(() => {
+      setRecipient(TEACHER)
 
       // Close modal
       cy.get('[data-test="closeModal"]').click()
@@ -38,42 +79,9 @@ describe('Warning', () => {
     cy.get('[data-test="warning-modal"]').should('be.exist')
     // Click on cancel button
     cy.get('[data-test="cancelButton"]').click()
-    // Check if warning modal is not visible
-    cy.get('[data-test="warning-modal"]').should('not.exist')
-    // Check if modal create modal is visible
     cy.get('[data-test="createMessageModal"]').should('be.visible')
   })
-  it('Messaging_DisplayWarningMessageOnMessageEdition_Modified_thread', function () {
-    // Login
-    cy.loadTables('messaging/messaging_tables.sql')
-    cy.login(HEADMASTER, messagingURL)
-    waitMessagingToBeLoaded()
-    const ModifiedDraftContent = this.messagingData.ModifiedDraftContent[0]
 
-    // Right click on first thread
-    cy.get('[data-test="threads-panel"]').within(() => {
-      cy.get('[data-test="thread-list-item"]').first().rightclick()
-    })
-    // Click on reply in options menu
-    cy.get('[data-test="reply"]').click()
-
-    // content modified
-    cy.get('[data-test="createMessageModal"]').within(() => {
-      cy.get('.ck-editor')
-      cy.type_ckeditor(ModifiedDraftContent[0].content)
-
-      // Close modal
-      cy.get('[data-test="closeModal"]').click()
-    })
-    // Check if warning modal is visible
-    cy.get('[data-test="warning-modal"]').should('be.exist')
-    // Click on validate button
-    cy.get('[data-test="confirmButton"]').click()
-    // Check if warning modal is not visible
-    cy.get('[data-test="warning-modal"]').should('not.exist')
-    // Check if modal create modal is not visible
-    cy.get('[data-test="createMessageModal"]').should('not.exist')
-  })
   it('Messaging_DisplayWarningMessageOnMessageEdition_UnModified_draft', function () {
     // Login
     cy.loadTables('messaging/messaging_tables.sql')
@@ -98,20 +106,72 @@ describe('Warning', () => {
     cy.get('[data-test="warning-modal"]').should('be.not.exist')
   })
 
-  it('Messaging_DisplayWarningMessageOnMessageEdition_UnModified_thread', function () {
+  it('Messaging_DisplayWarningMessageOnMessageEdition_UnModified_Reply', function () {
+    // Login
+    cy.loadTables('messaging/messaging_tables.sql')
+    cy.login(HEADMASTER, messagingURL)
+    waitMessagingToBeLoaded()
+    const secondThread = this.messagingData.existingThreads[1]
+
+    // Right click on first thread
+    getThread(secondThread).rightclick()
+    cy.get('[data-test="reply"]').click()
+
+    cy.get('[data-test="createMessageModal"]').within(() => {
+      // Close modal
+      cy.get('[data-test="closeModal"]').click()
+    })
+    // Check if warning modal is not visible
+    cy.get('[data-test="warning-modal"]').should('be.not.exist')
+  })
+
+  it('Messaging_DisplayWarningMessageOnMessageEdition_UnModified_ReplyAll', function () {
+    // Login
+    cy.loadTables('messaging/messaging_tables.sql')
+    cy.login(HEADMASTER, messagingURL)
+    waitMessagingToBeLoaded()
+    const secondThread = this.messagingData.existingThreads[1]
+
+    // Right click on first thread
+    getThread(secondThread).rightclick()
+    cy.get('[data-test="replyAll"]').click()
+
+    cy.get('[data-test="createMessageModal"]').within(() => {
+      // Close modal
+      cy.get('[data-test="closeModal"]').click()
+    })
+    // Check if warning modal is not visible
+    cy.get('[data-test="warning-modal"]').should('be.not.exist')
+  })
+
+  it('Messaging_DisplayWarningMessageOnMessageEdition_UnModified_Forward', function () {
+    // Login
+    cy.loadTables('messaging/messaging_tables.sql')
+    cy.login(HEADMASTER, messagingURL)
+    waitMessagingToBeLoaded()
+    const secondThread = this.messagingData.existingThreads[1]
+
+    // Right click on first thread
+    getThread(secondThread).rightclick()
+    cy.get('[data-test="forward"]').click()
+
+    cy.get('[data-test="createMessageModal"]').within(() => {
+      // Close modal
+      cy.get('[data-test="closeModal"]').click()
+    })
+    // Check if warning modal is not visible
+    cy.get('[data-test="warning-modal"]').should('be.not.exist')
+  })
+
+  it('Messaging_DisplayWarningMessageOnMessageEdition_UnModified_NewMessage', function () {
     // Login
     cy.loadTables('messaging/messaging_tables.sql')
     cy.login(HEADMASTER, messagingURL)
     waitMessagingToBeLoaded()
 
-    // Right click on first thread
-    cy.get('[data-test="threads-panel"]').within(() => {
-      cy.get('[data-test="thread-list-item"]').first().rightclick()
-    })
-    // Click on reply in options menu
-    cy.get('[data-test="reply"]').click()
+    // Click on new message
+    cy.get('[data-test="createMessageButton"]').click()
 
-    // content modified
     cy.get('[data-test="createMessageModal"]').within(() => {
       // Close modal
       cy.get('[data-test="closeModal"]').click()
