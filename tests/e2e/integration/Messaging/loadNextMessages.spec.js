edd1527550625a2012c5ac261302cae8c6210827
Evan <evanchauffour@gmail.com>
2023-10-26 11:23:18 +0200

    [tests - messaging] Change size large screen for pagination is smaller than screen size

--- tests/e2e/integration/Messaging/loadNextMessages.spec.js
+++ tests/e2e/integration/Messaging/loadNextMessages.spec.js
@@ -4,32 +4,8 @@ import { STUDENT } from '../../support/constants/users'
 import { waitMessagingToBeLoaded } from '../../support/utils/messagingUtils'
 
 describe('Load next message', () => {
-    // it('Create all messages', function () {
-    //   cy.loadTables('messaging/messaging_tables_empty.sql')
-    //   cy.login(HEADMASTER, messagingURL)
-    //   waitMessagingToBeLoaded()
-
-    //   const formattedLastName = STUDENT.lastName.charAt(0).toUpperCase() + STUDENT.lastName.slice(1).toLowerCase()
-    //   const formattedFirstName = STUDENT.firstName.charAt(0).toUpperCase() + STUDENT.firstName.slice(1).toLowerCase()
-
-    //   for (let i = 0; i < 25; i++) {
-    //     cy.get('[data-test="createMessageButton"]').click()
-    //     // Write message
-    //     cy.get('[data-test="createMessageModal"]').within(() => {
-    //       cy.get('.base-tags-input').type('penelope')
-    //       cy.get('.suggestion-list').contains(`${formattedLastName} ${formattedFirstName}`).click()
-    //       cy.get('.group > [data-test="subject-input"]').type(`Message ${i}`)
-    //       cy.get('.ck-editor')
-    //       cy.type_ckeditor(`Contenu du message ${i}`)
-
-    //       // Send message
-    //       cy.get('[data-test="submitButton"]').click()
-    //     })
-    //     cy.get('.close').click()
-    //   }
-    // })
-    const sizes = ['iphone-5', 'ipad-2', [1024, 768], [1024, 2000]]
-
+    const sizes = ['iphone-5', 'ipad-2', [1024, 768], [1024, 4000]]
+    const largeScreenNoScroll = sizes[3]
     sizes.forEach(size => {
       it(`Messaging_LoadNextMessages_on: ${size}`, function () {
         // Set testing viewport
@@ -45,13 +21,12 @@ describe('Load next message', () => {
         cy.login(STUDENT, messagingURL)
         waitMessagingToBeLoaded()
 
-        // Large screen
-        if (size === sizes[3]) {
+        // Pagination is smaller than screen size
+        if (size === largeScreenNoScroll) {
           // Check if all threads is visible
           cy.get('[data-test="thread-list-item"]').should('have.length', 25)
         } else {
-        // Normal screen
-
+        // Pagination is taller than screen size
         // Check if 20 threads is visible
         cy.get('[data-test="thread-list-item"]').should('have.length', 20)
         // Scroll to bottom
