3e83e075b2a2e8325900c4c9d95988908e90f42c
Evan <evanchauffour@gmail.com>
2023-10-26 10:29:26 +0200

    [tests - contactPicker] create function openTeachersList

--- tests/e2e/support/utils/contactPickerUtils.js
+++ tests/e2e/support/utils/contactPickerUtils.js
@@ -3,6 +3,16 @@ const openContactPickerSelection = () => {
   cy.get('[data-test="recipients-section"] > .base-button').click()
 }
 
+const openTeachersList = () => {
+  cy.get('[data-test="createMessageButton"]').click()
+  cy.get('[data-test="recipients-section"] > .base-button').click()
+  // Open list: "Personnels"
+  cy.get('.address-book').contains('button', 'Personnels').click()
+  // Click on teachers
+  cy.get('.address-book').contains('button', 'Enseignants·tes').click()
+}
+
 export {
-  openContactPickerSelection
+  openContactPickerSelection,
+  openTeachersList
 }
