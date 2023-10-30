e72cc14d3d3c1ad1ecb8f9e8ba6b5719aebc4fc4
Evan <evanchauffour@gmail.com>
2023-10-30 09:32:05 +0100

    [tests - contactPicker] Create test autoComplete and update contactPicker.json

---
+++ tests/e2e/integration/ContactPicker/autoComplete.spec.js
@@ -0,0 +1,54 @@
+import { messagingURL } from '../../support/constants/urls'
+import { HEADMASTER, PARENT, STUDENT } from '../../support/constants/users'
+
+describe('UserSelection_Autocomplete', () => {
+  beforeEach(() => {
+    cy.fixture('contactPicker.json').as('contactPickerData')
+  })
+
+  it('UserSelection_Autocomplete', function () {
+    const Contacts = this.contactPickerData.Contacts
+    // test autoCompletion
+    cy.login(HEADMASTER, messagingURL)
+    cy.get('[data-test="createMessageButton"]').click()
+
+    cy.get('.base-tags-input').type('pen')
+    cy.get('.suggestion-list').contains(`${STUDENT.lastName} ${STUDENT.firstName}`).should('be.exist')
+    cy.focused().clear()
+    cy.get('.base-tags-input').type(' pen')
+    cy.get('.suggestion-list').contains(`${STUDENT.lastName} ${STUDENT.firstName}`).should('be.exist')
+    cy.focused().clear()
+    cy.get('.base-tags-input').type('pén')
+    cy.get('.suggestion-list').contains(`${STUDENT.lastName} ${STUDENT.firstName}`).should('be.exist')
+    cy.focused().clear()
+    cy.get('.base-tags-input').type('Pen')
+    cy.get('.suggestion-list').contains(`${STUDENT.lastName} ${STUDENT.firstName}`).should('be.exist')
+    cy.focused().clear()
+
+    // STUDENT
+    cy.login(STUDENT, messagingURL)
+    cy.get('[data-test="createMessageButton"]').click()
+    for (let i = 0; i < Contacts.length - 1; i++) {
+      cy.get('.base-tags-input').type(Contacts[i].lastName)
+      if (Contacts[i].isVisibleStudent === true) {
+        cy.get('.suggestion-list').contains(Contacts[i].lastName).should('be.exist')
+      } else {
+        cy.get('.suggestion-list').contains(Contacts[i].lastName).should('not.exist')
+      }
+      cy.focused().clear()
+    }
+
+    // PARENT
+    cy.login(PARENT, messagingURL)
+    cy.get('[data-test="createMessageButton"]').click()
+    for (let i = 0; i < Contacts.length - 1; i++) {
+      cy.get('.base-tags-input').type(Contacts[i].lastName)
+      if (Contacts[i].isVisibleParent === true) {
+        cy.get('.suggestion-list').contains(Contacts[i].lastName).should('be.exist')
+      } else {
+        cy.get('.suggestion-list').contains(Contacts[i].lastName).should('not.exist')
+      }
+      cy.focused().clear()
+    }
+  })
+})
