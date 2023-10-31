1775c61f0b9814c8eaf2e73d246bc6ae0c41e4ca
Evan <evanchauffour@gmail.com>
2023-10-31 17:26:10 +0100

    [tests - contactPicker] Remove test to check tabs context

--- tests/e2e/integration/ContactPicker/searchUser.spec.js
+++ tests/e2e/integration/ContactPicker/searchUser.spec.js
@@ -1,6 +1,6 @@
 import { messagingURL } from '../../support/constants/urls'
-import { HEADMASTER, PARENT, STUDENT } from '../../support/constants/users'
-import { openContactPickerSelection, openTeachersList } from '../../support/utils/contactPickerUtils'
+import { HEADMASTER, STUDENT } from '../../support/constants/users'
+import { openContactPicker, openList } from '../../support/utils/contactPickerUtils'
 
 describe('UserSelection_AddressBook', () => {
   beforeEach(() => {
@@ -8,29 +8,21 @@ describe('UserSelection_AddressBook', () => {
   })
 
   it('UserSelection_AddressBook_SearchUser', function () {
-    // Check for a student
-    cy.login(STUDENT, messagingURL)
-    openContactPickerSelection()
-    cy.get('.tabs').should('not.exist')
-
-    // Check for a parent
-    cy.login(PARENT, messagingURL)
-    openContactPickerSelection()
-    cy.get('.tabs').should('not.exist')
-
-    // Check for an agent
+    // Login
     cy.login(HEADMASTER, messagingURL)
-    openTeachersList()
-    // Check if teachers list is visible
-    cy.get('.user-list-container').should('be.visible')
+
+    // Open contactPicker
+    openContactPicker()
     // Check if tab "Annuaire" is visible and click it
     cy.get('.tabs').within(() => {
       cy.contains('li', 'Annuaire').should('be.exist').click()
     })
 
     // Try search user
-    cy.get('.advanced-search > :nth-child(1) > .group > .labelled').type(STUDENT.firstName)
-    cy.get('button').contains('Rechercher').click()
+    cy.get('.advanced-search').within(() => {
+      cy.get('.group > [data-test="inputSearchAdvanceSearch"]').type(STUDENT.firstName)
+      cy.get('button').contains('Rechercher').click()
+    })
     // Check if user is find
     cy.get('.user-list-container').contains('li', STUDENT.firstName).should('be.exist')
 
@@ -40,26 +32,13 @@ describe('UserSelection_AddressBook', () => {
     cy.get('button').contains('Rechercher').click()
     // Check if user is not find
     cy.get('li').contains(`${STUDENT.lastName} ${STUDENT.firstName}`).should('not.exist')
-
-    // Change tab
-    cy.get('.tabs').within(() => {
-      cy.contains('Carnet d\'adresses').click()
-    })
-    // Check if content is reset
-    cy.get('.user-list-container').should('not.exist')
-    // Change tab
-    cy.get('.tabs').within(() => {
-      cy.contains('li', 'Annuaire').click()
-    })
-    // Check if content is reset
-    cy.get('.user-list-container').should('not.exist')
   })
 
   it('UserSelection_AddressBook_filterResultUser', function () {
     const FirstTeacherInList = this.contactPickerData.firstTeacherInList
 
     cy.login(HEADMASTER, messagingURL)
-    openTeachersList()
+    openList('Enseignants·tes')
 
     cy.get('.group > .filter-input').type('jack')
     cy.get('.user-list').contains('button', FirstTeacherInList.lastName).should('be.exist')
@@ -70,10 +49,6 @@ describe('UserSelection_AddressBook', () => {
     cy.focused().clear()
 
     cy.get('.group > .filter-input').type('JACK')
-    cy.get('.user-list').contains('button', FirstTeacherInList.lastName).should('be.exist')
-    cy.focused().clear()
-
-    cy.get('.group > .filter-input').type('Jack')
     cy.get('.user-list').contains('button', FirstTeacherInList.lastName).should('be.exist')
     cy.focused().clear()
 
