1b88503e5b4751ce5ad86cb5598ef560f61fcf42
Evan <evanchauffour@gmail.com>
2023-11-02 16:05:13 +0100

    [tests - dashboard] Create functions getEvent and getDetailEvent

--- tests/e2e/support/utils/dashboard.js
+++ tests/e2e/support/utils/dashboard.js
@@ -6,7 +6,21 @@ const getNews = (news) => {
   return cy.get('[data-test="announcement-widget"]').contains('.announcement', news.title)
 }
 
+const getEvent = (news) => {
+  return cy.get('[data-test="diary-widget"]').contains('.diary-event', news.title)
+}
+
+const getEventDetail = (event) => {
+  return cy.get('.detailed-event').within(() => {
+    cy.contains(event.content)
+    cy.contains(event.title)
+    cy.contains(event.location)
+  })
+}
+
 export {
   getNewsDetail,
-  getNews
+  getNews,
+  getEvent,
+  getEventDetail
 }
