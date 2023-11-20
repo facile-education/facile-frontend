145df962437c05a9591f474bdad6ecdf06fb1199
Evan <evanchauffour@gmail.com>
2023-11-20 11:12:57 +0100

    [tests - dashobard] Update display responsivity with get widget position

--- tests/e2e/integration/Dashboard/display.spec.js
+++ tests/e2e/integration/Dashboard/display.spec.js
@@ -2,7 +2,7 @@ import { dashboardURL } from '../../support/constants/urls'
 import { HEADMASTER, PARENT, SECRETARY, STUDENT, TEACHER } from '../../support/constants/users'
 
 describe('Dashboard_DisplayCorrectWidgets', () => {
-  it('Dashboard_DisplayCorrectWidgets_ByProfil_desktop', function () {
+  it('Dashboard_DisplayCorrectWidgets_ByProfilDesktop', function () {
     // For a student
     cy.login(STUDENT, dashboardURL)
     cy.get('[data-test="diary-widget"]').should('be.visible')
@@ -49,17 +49,19 @@ describe('Dashboard_DisplayCorrectWidgets', () => {
     cy.get('[data-test="statistics-widget"]').should('not.exist')
   })
 
-  it('Dashboard_DisplayCorrectWidgets_Test_Responsivity', function () {
+  it.only('Dashboard_DisplayCorrectWidgets_Responsivity', function () {
     cy.viewport('iphone-5')
     // Test Responsivity
     cy.login(STUDENT, dashboardURL)
     cy.get('[data-test="diary-widget"]').should('be.visible')
     cy.get('[data-test="activity-widget"]').should('be.visible')
     cy.get('[data-test="announcement-widget"]').should('be.visible')
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
-    cy.get('[data-test="schedule-widget"] > header > nav').scrollIntoView().should('be.visible')
-    // eslint-disable-next-line cypress/unsafe-to-chain-command
-    cy.get('[data-test="homeWork-widget"]').scrollIntoView().should('be.visible')
-    cy.get('[data-test="statistics-widget"]').should('not.exist')
+    cy.get('[data-test="schedule-widget"]').should('be.visible')
+    // Assert that homework widget is below the bottom of the viewport
+    cy.get('[data-test="homeWork-widget"]').then(($homeWorkWidget) => {
+      // Get homwork widget position
+      const homeWorkWidgetTop = $homeWorkWidget[0].getBoundingClientRect().top
+      expect(homeWorkWidgetTop).to.be.greaterThan(Cypress.config('viewportHeight'))
+    })
   })
 })
