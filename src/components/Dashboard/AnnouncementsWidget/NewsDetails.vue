ae09ad1d9ebadda5556160b749ff09c8464b62e7
vdona <vdona@pentila.com>
2023-11-24 16:22:09 +0100

    [Dashboard] Fix default images for announcements and news

--- src/components/Dashboard/AnnouncementsWidget/NewsDetails.vue
+++ src/components/Dashboard/AnnouncementsWidget/NewsDetails.vue
@@ -158,7 +158,7 @@ export default {
     },
     thumbnail () {
       if (defaultImagesKeys.indexOf(this.detailedNews.thumbnailUrl) !== -1) {
-        return new URL(`../../../assets/images/${this.detailedNews.thumbnailUrl}.png`, import.meta.url).href
+        return new URL(`../../../assets/images/${this.detailedNews.thumbnailUrl}.svg`, import.meta.url).href
       } else { // Returned url is a key for local default image
         return this.detailedNews.thumbnailUrl
       }
