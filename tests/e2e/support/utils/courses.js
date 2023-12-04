d2f1aa74afb9337e8dfbf531da02d61dde71e123
Evan <evanchauffour@gmail.com>
2023-12-04 17:02:51 +0100

    [tests - C&D] Create function to get sessionDetail and update Json with existing sessions

--- tests/e2e/support/utils/courses.js
+++ tests/e2e/support/utils/courses.js
@@ -1,14 +1,12 @@
 const getHomework = (homework) => {
   return cy.contains('.homework', homework.title).within(() => {
     cy.contains(homework.content)
-    cy.contains(homework.teacher)
   })
 }
 
-const getHomeworkDetailsContents = (homework) => {
+const getHomeworkDetailsSupport = (homework) => {
   return cy.contains('.homework', homework.title).within(() => {
     cy.contains(homework.content)
-    cy.contains(homework.teacher)
     cy.get('.attached-file').should('contain', homework.attachedFile)
     cy.get('.ck-editor').should('contain', homework.additionalText)
     cy.get('.course-content').eq(0).should('contain', homework.audio)
@@ -18,7 +16,42 @@ const getHomeworkDetailsContents = (homework) => {
   })
 }
 
+const getSession = (session) => {
+  return cy.contains('.session-content', session.title).within(() => {
+    cy.contains(session.content)
+  })
+}
+
+const getSessionDetailsSupport = (sessionSupport) => {
+  return cy.contains('.session-content', sessionSupport.title).within(() => {
+    cy.contains(sessionSupport.content)
+    cy.get('.attached-file').should('contain', sessionSupport.attachedFile)
+    cy.get('.ck-editor').should('contain', sessionSupport.additionalText)
+    cy.get('.course-content').eq(0).should('contain', sessionSupport.audio)
+    cy.get('.course-content').eq(1).should('contain', sessionSupport.link)
+    cy.get('.course-content').eq(2).should('contain', sessionSupport.video)
+    cy.get('.course-content').eq(3).should('contain', sessionSupport.h5p)
+  })
+}
+
+const getCourseSessionDetails = (homework, session) => {
+  return cy.contains('.session-infos', session.headText).within(() => {
+    getHomework(homework)
+  })
+}
+
+const getCourseSessionDetailsSupport = (homework, session, sessionSupport) => {
+  return cy.contains('.session-infos', session.headText).within(() => {
+    getHomeworkDetailsSupport(homework)
+    getSessionDetailsSupport(sessionSupport)
+  })
+}
+
 export {
   getHomework,
-  getHomeworkDetailsContents
+  getHomeworkDetailsSupport,
+  getSession,
+  getSessionDetailsSupport,
+  getCourseSessionDetails,
+  getCourseSessionDetailsSupport
 }
