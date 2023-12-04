d947702162a71384039bf97268b8c40283bba959
Evan <evanchauffour@gmail.com>
2023-12-04 14:00:00 +0100

    [tests C&D] Create function to check homeworks details and update json

--- tests/e2e/support/utils/courses.js
+++ tests/e2e/support/utils/courses.js
@@ -5,6 +5,20 @@ const getHomework = (homework) => {
   })
 }
 
+const getHomeworkDetailsContents = (homework) => {
+  return cy.contains('.homework', homework.title).within(() => {
+    cy.contains(homework.content)
+    cy.contains(homework.teacher)
+    cy.get('.attached-file').should('contain', homework.attachedFile)
+    cy.get('.ck-editor').should('contain', homework.additionalText)
+    cy.get('.course-content').eq(0).should('contain', homework.audio)
+    cy.get('.course-content').eq(1).should('contain', homework.link)
+    cy.get('.course-content').eq(2).should('contain', homework.video)
+    cy.get('.course-content').eq(3).should('contain', homework.h5p)
+  })
+}
+
 export {
-  getHomework
+  getHomework,
+  getHomeworkDetailsContents
 }
