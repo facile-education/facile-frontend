870b1049385de6e1910ffbfc9ee28acd8e120dd0
Evan <evanchauffour@gmail.com>
2023-12-05 12:10:56 +0100

    [tests C&D] Create tests to create and delete a sessionContent

---
+++ tests/e2e/integration/Courses/sessionContent.spec.js
@@ -0,0 +1,125 @@
+import { coursesURL } from '../../support/constants/urls'
+import { TEACHER } from '../../support/constants/users'
+import { getSessionContent, getSessionContentWithSupport } from '../../support/utils/courses'
+
+describe('Sessions content', () => {
+  beforeEach(() => {
+    cy.loadTables('courses/courses_tables_homework.sql')
+    cy.fixture('courses.json').as('coursesData')
+  })
+  it('Courses_CreateSessionContent_CreateWithAllContentsTypes', function () {
+    const sessionContentToCreate = [
+      {
+        title: 'Support de cours créé',
+        content: 'Description du support de cours créé',
+        attachedFile: 'note.html',
+        additionalText: 'Texte supplémentaire pour le support de cours',
+        audio: 'Audio du support de cours',
+        link: 'Lien pour le support de cours',
+        linkUrl: 'https://www.monliencréé.com',
+        video: 'Vidéo pour le support de cours',
+        videoUrl: '<iframe width="560" height="315" src="https://www.youtube.com/embed/BQCSn5YMc6s?si=r-O2MkvB1QSCqRpr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        h5p: 'H5P pour le support de cours',
+        h5pUrl: '<iframe src="https://h5p.org/h5p/embed/617" width="1090" height="675" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" title="Interactive Video"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>'
+      }
+    ]
+    // Login
+    cy.login(TEACHER, coursesURL)
+    cy.clock().invoke('setSystemTime', Cypress.dayjs('2023/09/11', 'YYYY/MM/DD').toDate().getTime()) // To put after login to make it works
+    // Click on session
+    cy.get('[data-test="09-11_11:25"]').click()
+    cy.get('.session-content').within(() => {
+      cy.get('[data-test="createSessionContent"]').click()
+    })
+    cy.get('.edit-course-modal').should('be.visible').within(() => {
+      // Set title
+      cy.get('.labelled').type(sessionContentToCreate[0].title)
+      // Set description
+      cy.get('.ck-editor')
+      cy.type_ckeditor(sessionContentToCreate[0].content)
+      // Add cy.tick beacause cy.clock()
+      cy.tick(1000)
+    })
+    // Add attached file
+    cy.get('[title="Ajouter une pièce jointe depuis vos documents de l\'ENTA"]').click()
+    cy.get('[data-test="documentFile"]').within(() => {
+      cy.contains('.file', sessionContentToCreate[0].attachedFile).click()
+      cy.get('[data-test="submitButton"]').click()
+    })
+
+    // Add texte
+    cy.get('[title="Ajouter du texte"]').click()
+    cy.get('[data-test="additionalText"]').eq(1)
+    cy.type_ckeditor(sessionContentToCreate[0].additionalText)
+    // Add cy.tick beacause cy.clock()
+    cy.tick(1000)
+
+    // Skip test audio because bug cy.clock()
+
+    // Add link
+    cy.get('[title="Ajouter un lien"]').click()
+    cy.get('[data-test="link"]').within(() => {
+      cy.get('.link-name').type(sessionContentToCreate[0].link)
+      cy.get('.link-url').type(sessionContentToCreate[0].linkUrl)
+      cy.get('[data-test="addLinkButton"]').click()
+    })
+
+    // Add video
+    cy.get('[title="Ajouter une vidéo"]').click()
+    cy.get('[data-test="video"]').within(() => {
+      cy.get('.video-name').type(sessionContentToCreate[0].video)
+      cy.get('.video-url').type(sessionContentToCreate[0].videoUrl)
+      cy.get('[data-test="addVideo"]').click()
+    })
+
+    // Add H5P
+    cy.get('[title="Ajouter un élément H5P"]').click()
+    cy.get('[data-test="h5p"]').within(() => {
+      cy.get('.content-name').type(sessionContentToCreate[0].h5p)
+      cy.get('.content-url').type(sessionContentToCreate[0].h5pUrl)
+      cy.get('[data-test="addH5P"]').click()
+    })
+
+    // Click on button to create session content
+    cy.get('.edit-course-modal').should('be.visible').within(() => {
+      cy.contains('button', 'Publier').click()
+    })
+    // Wait to load session content
+    cy.intercept('GET', '**/get-session-contents?**').as('loadSessionContent')
+    cy.wait('@loadSessionContent')
+    // Check if session content is visible
+    cy.get('.session-details').within(() => {
+      // Check if session content and session homework is visible
+      getSessionContentWithSupport(sessionContentToCreate[0]).should('be.visible')
+    })
+  })
+
+  it('Courses_DeleteSessionContent_DeleteFromSessionViewAndCourseView', function () {
+    const currentSessionContent = this.coursesData.existingSessionsContent[0]
+    // Login
+    cy.login(TEACHER, coursesURL)
+    cy.clock().invoke('setSystemTime', Cypress.dayjs(currentSessionContent.sessionDate, 'YYYY/MM/DD').toDate().getTime()) // To put after login to make it works
+
+    // Click on session
+    cy.get('[data-test="11-13_11:25"]').click()
+    cy.get('.session-details').within(() => {
+      // Get current session content
+      cy.contains('.session-content', currentSessionContent.title).within(() => {
+        cy.get('.content-title').within(() => {
+          // Open edit session content panel
+          cy.get('.edit-button').click()
+        })
+      })
+    })
+    // Click on delete button
+    cy.get('.context-menu').within(() => {
+      cy.contains('button', 'Supprimer').click()
+    })
+    cy.get('.session-details').within(() => {
+      // Check if no session content is visible
+      getSessionContent(currentSessionContent).should('not.exist')
+      // Check if placeholder is visible
+      cy.contains('.placeholder', 'Aucun support de cours enregistré').should('be.visible')
+    })
+  })
+})
