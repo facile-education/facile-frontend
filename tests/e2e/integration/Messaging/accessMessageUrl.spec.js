3270c9ec42b389664bea03729c08a52f46cb1c7b
vdona <vdona@pentila.com>
2023-12-06 17:26:04 +0100

    [Tests] Fix messaging reload after url message display and associated test

--- tests/e2e/integration/Messaging/accessMessageUrl.spec.js
+++ tests/e2e/integration/Messaging/accessMessageUrl.spec.js
@@ -75,11 +75,10 @@ describe('Messaging_AccessMessageViaURL', () => {
     cy.get('[data-test=messages-panel]').should('be.visible')
     // Back to threads panel
     cy.get('[data-test="option_back"]').click()
-    // Check if thirdThread exist
+    // Check if all threads are visible
     cy.get('[data-test="threads-panel"]').within(() => {
       getThread(thirdThread).should('be.exist')
+      cy.get('[data-test="thread-list-item"]').should('have.length', 4)
     })
-    // Check if just one thread is visible
-    cy.get('[data-test="thread-list-item"]').should('have.length', 1)
   })
 })
