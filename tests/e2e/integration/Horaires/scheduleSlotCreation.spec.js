ca94f12ca3ab58358e130d858730379c81ebcd5c
vdona <vdona@pentila.com>
2023-12-15 11:41:51 +0100

    [Schedule - Tests] Create slot tests + add room validation

--- tests/e2e/integration/Horaires/scheduleSlotCreation.spec.js
+++ tests/e2e/integration/Horaires/scheduleSlotCreation.spec.js
@@ -1,6 +1,6 @@
 import { scheduleURL } from '../../support/constants/urls'
 import { DOYEN, HEADMASTER, PARENT, SCHOOL_ADMIN, SECRETARY, STUDENT, TEACHER } from '../../support/constants/users'
-import { waitScheduleToBeLoaded } from '../../support/utils/scheduleUtils'
+import { checkScheduleSlot, getSlot, waitScheduleToBeLoaded } from '../../support/utils/scheduleUtils'
 import { selectAutocompleteItem, selectDropdownItem } from '../../support/utils/testUtils'
 
 const scheduleUsers = [HEADMASTER, SCHOOL_ADMIN, DOYEN, SECRETARY, TEACHER, STUDENT, PARENT]
@@ -26,10 +26,13 @@ const fillSessionModal = (slotToCreate) => {
   selectDropdownItem('[data-test="subject-dropdown"]', slotToCreate.courseSubject)
 
   // Slot teacher
-  selectAutocompleteItem('[data-test="subject-dropdown"] > [placeholder="Enseignant"]', slotToCreate.teacher.firstName + ' ' + slotToCreate.teacher.firstName)
+  slotToCreate.teachers.forEach(teacher => {
+    cy.get('[data-test="tagsInput"]').as('teacherTagsInput') // TODO: Change selector to get the tags input that contain [placeholder="Enseignants"] property
+    selectAutocompleteItem('@teacherTagsInput', slotToCreate.teacher.lastName + ' ' + slotToCreate.teacher.firstName)
+  })
 
   // Slot room
-  cy.get('input[placeholder="Salle"').type(slotToCreate.room)
+  cy.get('input[placeholder="Salle"]').type(slotToCreate.room)
 
   // Slot recurrence
   if (slotToCreate.isRecurrent) {
@@ -50,7 +53,7 @@ describe('Schedule_CreateSlot', () => {
     cy.intercept('GET', '**/schedule.cdtsession/get-user-sessions**').as('getUserSessions')
   })
 
-  context('Check display', () => {
+  context('Check display', function () {
     it('Schedule_CreateSlot_CheckDisplayForRole', () => {
       scheduleUsers.forEach(user => {
         cy.log('Check display for role ' + user.role)
@@ -87,30 +90,116 @@ describe('Schedule_CreateSlot', () => {
     })
   })
 
-  // todo: load schedule tables on the create tests context
-  it('Schedule_CreateSlot_CreateSlot', () => {
+  it.skip('Schedule_CreateSlot_CreateSlot', function () { // TODO: unskip when the date is handle by the frontend
+    const weekListFormat = 'D MMM'
+    const currentWeek = Cypress.dayjs(this.scheduleData.now, 'YYYY/MM/DD HH:mm')
+    const nextWeekLabel = currentWeek.add(1, 'week').startOf('week').format(weekListFormat)
     const slotToCreate = {
       schoolSlotNumber: 'P2',
       startDate: '2023/10/02 08:45',
       endDate: '2023/10/02 09:30',
-      teacher: TEACHER,
+      teachers: [TEACHER],
       room: 'te57',
       courseName: 'AL0922',
       courseSubject: 'Allemand',
-      groupName: this.scheduleData.selectedGroup.groupName,
+      groupName: this.scheduleData.selectedGroup.name,
       isRecurrent: false
     }
+    const secondSlotToCreate = {
+      schoolSlotNumber: 'P4',
+      startDate: '2023/10/02 10:35',
+      endDate: '2023/10/02 11:20',
+      teachers: [TEACHER, DOYEN],
+      room: 'te57',
+      courseName: 'AL0922',
+      courseSubject: 'Allemand',
+      groupName: this.scheduleData.selectedGroup.name,
+      isRecurrent: true
+    }
+
+    cy.loadTables('schedule/schedule_tables_empty.sql')
 
     cy.login(canCreateSlotUsers[0], scheduleURL)
+
     waitScheduleToBeLoaded()
 
+    // Create first slot
     cy.contains('button', 'NOUVEAU').click()
     cy.get('[data-test="create-session-modal"]').within(() => {
       fillSessionModal(slotToCreate)
 
       cy.contains('button', 'Créer').click()
     })
+    cy.get('[data-test="create-session-modal"]').should('not.exist')
+
+    // Group dropdown selection is updated with the selected class
+    cy.contains('[data-test="dropdown"]', this.scheduleData.selectedGroup.name)
+
+    // Check calendar content
+    cy.get('.fc-timegrid-event').should('have.length', this.scheduleData.selectedGroup.nbWeekSlots + 1)
+    checkScheduleSlot(slotToCreate)
+
+    // Create second slot (recurrent with multi-teachers)
+    cy.contains('button', 'NOUVEAU').click()
+    cy.get('[data-test="create-session-modal"]').within(() => {
+      fillSessionModal(secondSlotToCreate)
+
+      cy.contains('button', 'Créer').click()
+    })
+    cy.get('[data-test="create-session-modal"]').should('not.exist')
+
+    // Group dropdown selection is updated with the selected class
+    cy.contains('[data-test="dropdown"]', this.scheduleData.selectedGroup.name)
+
+    // Check calendar content
+    cy.get('.fc-timegrid-event').should('have.length', this.scheduleData.selectedGroup.nbWeekSlots + 2)
+    checkScheduleSlot(secondSlotToCreate)
+
+    // Go one week after
+    cy.get('[data-test="weekList"]').within(() => {
+      cy.contains('button', nextWeekLabel).click()
+    })
+
+    // Check slots
+    cy.get('.fc-timegrid-event').should('have.length', this.scheduleData.selectedGroup.nbWeekSlots + 1)
+    getSlot(slotToCreate).should('not.exist')
+    checkScheduleSlot({
+      ...secondSlotToCreate,
+      startDate: Cypress.dayjs(secondSlotToCreate.startDate, 'YYYY/MM/DD HH:mm').add(1, 'week'),
+      endDate: Cypress.dayjs(secondSlotToCreate.endDate, 'YYYY/MM/DD HH:mm').add(1, 'week')
+    })
   })
 
-  // modale initialisation (depend of selected group, current week
+  it.skip('Schedule_CreateSlot_CreateSlotWithConflicts', function () {
+    // Create new slot at the same moment as an other slot with same teachers or same room
+  })
+
+  it.only('Schedule_CreateSlot_CheckModalInitialisationAndValidation', function () {
+    cy.intercept('GET', '**/schedule.cdtsession/get-group-sessions**').as('getGroupSessions')
+
+    cy.login(canCreateSlotUsers[0], scheduleURL)
+
+    waitScheduleToBeLoaded()
+
+    // Select a class
+    cy.contains('[data-test="dropdown"]', 'Groupe').click()
+    cy.contains('.suggestion-list > li', this.scheduleData.selectedGroup.name).click()
+    cy.wait('@getGroupSessions')
+
+    cy.contains('button', 'NOUVEAU').click()
+    cy.get('[data-test="create-session-modal"]').within(() => {
+      // Slot group
+      cy.get('[data-test="group-dropdown"]').should('contain', this.scheduleData.selectedGroup.name)
+      // No particular other init behaviour to test
+
+      // Form validation
+      cy.contains('button', 'Créer').click()
+      cy.get('.teachers').within(() => {
+        cy.contains('.error-message', 'Veuillez sélectionner au moins un enseignant')
+      })
+      cy.get('.room').within(() => {
+        cy.contains('.error-message', 'Champ requis')
+      })
+    })
+  })
 })
--- tests/e2e/support/utils/testUtils.js
+++ tests/e2e/support/utils/testUtils.js
@@ -3,13 +3,13 @@ const exactString = (string) => {
 }
 
 const selectDropdownItem = (dropdownSelector, itemName) => {
-  cy.get(dropdownSelector + '> button').click()
+  cy.get(dropdownSelector + ' > button').click()
   cy.get(dropdownSelector).contains('.suggestion-list > li', itemName).click()
 }
 
 const selectAutocompleteItem = (autocompleteSelector, itemName) => {
-  cy.get(autocompleteSelector + '> input').clear()
-  cy.get(autocompleteSelector + '> input').type(itemName)
+  cy.get(autocompleteSelector).find('input').clear()
+  cy.get(autocompleteSelector).find('input').type(itemName)
   cy.get(autocompleteSelector).contains('.suggestion-list > li', itemName).click()
 }
 
