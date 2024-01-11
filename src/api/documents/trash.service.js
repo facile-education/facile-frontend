e6b595d596634afdb8d6d12e4e54cadc6109905f
vdona <vdona@pentila.com>
2024-01-11 17:43:03 +0100

    [Documents] Fix bad delete files axios formatting

--- src/api/documents/trash.service.js
+++ src/api/documents/trash.service.js
@@ -1,4 +1,3 @@
-import WeprodeUtils from '@utils/weprode.utils'
 import axios from 'axios'
 
 import constants from '@/api/constants'
@@ -13,8 +12,10 @@ const TRASH_PATH = '/document.trash'
  * Delete definitely the specified entities from the document library (and return a list of the failed entities if some failed)
  */
 function deleteEntities (folderIdArray, fileIdArray) {
-  return axios.delete(constants.JSON_WS_URL + TRASH_PATH + '/delete-documents', WeprodeUtils.params({
-    folderIdArray: JSON.stringify(folderIdArray),
-    fileIdArray: JSON.stringify(fileIdArray)
-  })).then(response => response.data)
+  return axios.delete(constants.JSON_WS_URL + TRASH_PATH + '/delete-documents', {
+    params: {
+      folderIdArray: JSON.stringify(folderIdArray),
+      fileIdArray: JSON.stringify(fileIdArray)
+    }
+  }).then(response => response.data)
 }
