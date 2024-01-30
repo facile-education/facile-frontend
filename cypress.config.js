642070958846d6920343943f1a7cfe4d9a34abc7
vdona <vdona@pentila.com>
2024-01-30 18:12:59 +0100

    [Documents - Tests] Add download file POC test

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress')
+const { rmdir, access } = require('fs').promises
 
 // NB: To directly open the board with all specs and save time, run
 // $ cypress open --browser chrome --e2e
@@ -6,18 +7,32 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   fixturesFolder: 'tests/e2e/fixtures',
+  downloadsFolder: 'tests/e2e/downloads',
   screenshotsFolder: 'tests/e2e/screenshots',
   videosFolder: 'tests/e2e/videos',
   watchForFileChanges: false,
 
   e2e: {
     baseUrl: 'https://dev-ent-gve.com',
-    // setupNodeEvents (on, config) {
-    //   // bind to the event we care about
-    //   on('<event>', (arg1, arg2) => {
-    //     // plugin stuff here
-    //   })
-    // },
+    setupNodeEvents (on, config) {
+      // bind to the event we care about
+      on('task', {
+        async deleteFolder (folderName) {
+          console.log('deleting folder ' + folderName)
+          try {
+            // Check if the folder exists
+            await access(folderName)
+            // If the folder exists, proceed to delete
+            await rmdir(folderName, { maxRetries: 10, recursive: true })
+            console.log('Folder deleted successfully.')
+          } catch (err) {
+            // Handle the case when the folder doesn't exist
+            console.warn(`Folder ${folderName} does not exist. Nothing to delete.`)
+          }
+          return null
+        }
+      })
+    },
     specPattern: 'tests/e2e/**/*.spec.{js,jsx,ts,tsx}',
     // excludeSpecPattern: import.meta.env.CI ? ['tests/e2e/**/all.spec.js'] : [],
     supportFile: 'tests/e2e/support/index.js',
