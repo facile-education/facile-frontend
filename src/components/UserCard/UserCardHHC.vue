5ca749b0e87a5a97957cccef07298c5e910bb3f2
Evan <evanchauffour@gmail.com>
2024-02-19 18:23:48 +0100

    [UserCard] Create HHC section

---
+++ src/components/UserCard/UserCardHHC.vue
@@ -0,0 +1,81 @@
+<template>
+  <section
+    class="containerUserCardHHC"
+    data-test="userCardHHC"
+  >
+    <h2>
+      <CustomIcon
+        class="hhc-icon"
+        :icon-name="'icon-clock'"
+      />
+      <span>{{ $t('hhcTitle') }} :</span>
+    </h2>
+    <ul class="content">
+      <li
+        v-for="(item, index) in userSchoolLifeStats"
+        :key="index"
+      >
+        {{ slotType(item.type).label }}: {{ item.number }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+import notUsualSlotsConstants from '@/constants/notUsualSlots'
+const CustomIcon = defineAsyncComponent(() => import('@components/Base/CustomIcon.vue'))
+export default {
+  name: 'UserCardHHC',
+  components: {
+    CustomIcon
+  },
+  props: {
+    userSchoolLifeStats: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    slotType (slotType) {
+      return notUsualSlotsConstants.getSlotTypeByNumber(slotType)
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "@design";
+.containerUserCardHHC {
+  margin-top: 32px;
+  h2 {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    margin: 0;
+    margin-bottom: 8px;
+    @extend %font-regular-l;
+  }
+  .hhc-icon{
+    font-size: 1.5rem;
+  }
+}
+.content {
+  margin-left: 16px;
+  padding: 0;
+  li{
+    list-style: none;
+    @extend %font-regular-m;
+    margin-bottom: 5px;
+  }
+}
+</style>
+
+<i18n locale="fr">
+    {
+      "hhcTitle": "Horaires hors cadre",
+    }
+</i18n>
