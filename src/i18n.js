4a9cc3ce75b59733e0ae135dccfb4148938ec140
vdona <vdona@pentila.com>
2024-03-06 16:41:47 +0100

    [i18n] Use browser locale for public pages

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,8 @@ import messages from '@intlify/unplugin-vue-i18n/messages'
 import { createI18n } from 'vue-i18n'
 
 const i18n = createI18n({
-  locale: import.meta.env.VITE_VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: import.meta.env.VITE_VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: navigator.language || import.meta.env.VITE_VUE_APP_I18N_LOCALE || 'en',
+  fallbackLocale: navigator.language || import.meta.env.VITE_VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages
 })
 
