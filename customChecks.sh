ebd121d3009e36a881779f603fad64079d471756
vdona <vdona@pentila.com>
2024-03-22 10:13:21 +0100

    [i18n] Add script to check line number in translation files + fix english one

---
+++ customChecks.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+declare -a languages=("fr" "en" "it" "de")
+
+locale_folder="src/locales"
+
+# Check if 'locales' folder exist
+if [ ! -d "$locale_folder" ]; then
+  echo "Folder $locale_folder does not exist."
+  exit 1
+fi
+
+# /!\ The file must end with a new line to count the last translation key
+function count_translation_key_in_file {
+  local file="$1"
+  local nb_key=0
+
+  # Counting the lines containing at least 3 characters
+  while IFS= read -r ligne; do
+    if [ ${#ligne} -ge 3 ]; then
+      ((nb_key++))
+    fi
+  done < "$file"
+
+  echo "$nb_key"
+}
+
+function count_lang_key {
+  local folder="$1"
+  local lang="$2"
+  local file="${lang}.json"
+
+  if [ -f "$folder/$file" ]; then
+    count_translation_key_in_file "$folder/$file"
+  else
+    echo "0"
+  fi
+}
+
+# Check line number in translation files
+for language in "${languages[@]}"; do
+  current_lang_nb_key=$(count_lang_key "$locale_folder" "$language")
+  echo "$current_lang_nb_key translation key for language $language"
+done
