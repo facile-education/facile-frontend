441addd4d99842eb344d973894c3230c55c69f27
vdona <vdona@pentila.com>
2024-04-05 17:32:25 +0200

    Remove unused App constants

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,24 +1,13 @@
 // The keys should match the one in fr.json "Menu"
-const ACCESS_MANAGER = 'access-admin'
-const APPLICATION_MANAGER = 'application-admin'
+// TODO: Review usage of those statics route name and check if it's translation-compliant
 const COURSES = 'cdt'
-const CORTEX = 'cortex'
 const DASHBOARD = 'dashboard'
-const DASHBOARD_MANAGER = 'dashboard-admin'
-const DISCIPLINES_CO = 'disciplines-co'
 const DOCUMENTS = 'documents'
 const GROUPS = 'groups'
-const GRR = 'grr'
-const H5P = 'h5p'
 const SCHEDULE = 'horaires'
-const SCHEDULE_MANAGER = 'horaires-admin'
 const SCHOOL_LIFE = 'horaires-hors-cadre'
-const MAINTENANCE = 'maintenance'
 const MESSAGING = 'messaging'
 const STATISTICS = 'statistics'
-const UNIVERSALIS = 'universalis'
-const UNIVERSALIS_JUNIOR = 'universalis-junior'
-const USER_MANAGER = 'user-admin'
 
 const nbCharBeforeCompletion = 2
 const timeBeforeCompletion = 500
@@ -84,26 +73,14 @@ export {
   fileAutoSaveTime,
   entityNameMaxSize,
   ckMaxSize,
-  ACCESS_MANAGER,
-  APPLICATION_MANAGER,
   COURSES,
-  CORTEX,
   DASHBOARD,
-  DASHBOARD_MANAGER,
-  DISCIPLINES_CO,
   DOCUMENTS,
   GROUPS,
-  GRR,
-  H5P,
   SCHEDULE,
-  SCHEDULE_MANAGER,
   SCHOOL_LIFE,
-  MAINTENANCE,
   MESSAGING,
-  STATISTICS,
-  UNIVERSALIS,
-  UNIVERSALIS_JUNIOR,
-  USER_MANAGER
+  STATISTICS
 }
 
 export default {
